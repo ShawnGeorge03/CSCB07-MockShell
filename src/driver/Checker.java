dd9f77a506af9626230b6276157e307da814378e
PatelAbhay <patel.abhay1@hotmail.com>
2020-06-23 20:04:15 +0000

    Implemented valid_command for checker class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@61 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Checker.java
+++ src/driver/Checker.java
@@ -1,42 +1,69 @@
 package driver;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Checker {
   
   public String userInput;
-  //private ErrorMessage error;
-  //private Command valid_commands;
-  
-  /*
-   * Idea:
-   * Make a linked list, or use an ArrayList of commands that are valid so we can check if the input
-   * given by user match any of the commands that have been implemented
-   * 
-   */
+  //temp arraylist
+  private ArrayList<String> implemented_commands = new ArrayList<String>();
   
   public Checker() {
     this.userInput = "";
-    //make ErrorMessage null or something
+    fill_commands();
+  }
+  
+  private void fill_commands() {
+    this.implemented_commands.add("ls");
+    this.implemented_commands.add("mkdir");
+    this.implemented_commands.add("exit");
+    this.implemented_commands.add("history");
+    this.implemented_commands.add("speak");
+    this.implemented_commands.add("pwd");
+    this.implemented_commands.add("cd");
+    this.implemented_commands.add("cat");
+    this.implemented_commands.add("mv");
+    this.implemented_commands.add("pushd");
+    this.implemented_commands.add("popd");
+    this.implemented_commands.add("echo");
+    this.implemented_commands.add("man");
   }
   
   public String parseInput(String input) {
     return input.trim().replaceAll("\\s+", " ");
   }
   
-  //work on implementing after the command class is done
   public boolean isValidCommand(String input) {
-    return true;
+    String command_name = input.split(" ")[0];
+    
+    if(implemented_commands.contains(command_name)) return true;
+    
+    return false;
+  }
+  
+  public void add_command(String command_name) {
+    implemented_commands.add(command_name);
   }
   
   //Main method for testing code
   public static void main(String[] args) {
     Checker test = new Checker();
     Scanner input = new Scanner(System.in);
-    while(!test.userInput.equals("exit")) {
+    String u_input;
+    while(true) {
       System.out.print(">>>");
-      test.userInput = input.nextLine().trim().replaceAll("\\s+", " ");
-      System.out.println(test.userInput);
+      u_input = input.nextLine().trim().replaceAll("\\s+", " ");
+      if(test.isValidCommand(u_input))
+      {
+        System.out.println("VALID");
+        if(u_input.equals("exit")) break;
+        
+      }
+      else 
+      {
+        System.out.println("THIS IS NOT A VALID COMMAND");
+      }
     }
   }
 
