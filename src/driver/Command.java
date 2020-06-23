cdd8aa431789380d44348247476b69a1047179eb
ProgramTP <tirthpatelx@gmail.com>
2020-06-23 20:03:40 +0000

    changed arguments to arraylist
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@60 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Command.java
+++ src/driver/Command.java
@@ -1,11 +1,13 @@
 package driver;
 
+import java.util.ArrayList;
+
 public class Command {
 
   String command;
-  String[] arguments;
+  ArrayList<String> arguments = new ArrayList<String>();
   
-  public Command(String command, String[] arguments) {
+  public Command(String command, ArrayList<String> arguments) {
     this.command = command;
     this.arguments = arguments;
   }
