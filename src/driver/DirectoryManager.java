e5aa4d5afdaac0c8883fcf67dd2cd063e1518ada
ProgramTP <tirthpatelx@gmail.com>
2020-06-25 19:34:11 +0000

    some methods for DirectoryManager
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@78 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/DirectoryManager.java
+++ src/driver/DirectoryManager.java
@@ -1,5 +1,46 @@
 package driver;
 
+import java.util.ArrayList;
+
 public class DirectoryManager {
+  
+  FileSystem filesys = null;
+  ArrayList<String> path = new ArrayList<String>();
+  
+  public DirectoryManager() {
+    filesys = FileSystem.getFileSys();
+  }
+  
+  public String getCurrentPath() {
+    String output = "/";
+    Node curr = filesys.getCurrent();
+    if (curr.name == "/") {
+      return "/";
+    }
+    path.add(curr.name);
+    while (curr.name != "/") {
+      curr = curr.parent;
+      path.add(curr.name);
+    }
+    
+    int i = path.size();
+    while (i > 0) {
+      output = output.concat(path.get(i-1) + "/");
+      i--;
+    }
+
+    return output.substring(0,output.length()-1);
+  }
+  
+  public Node makePath(String path) {
+    String[] pathArr = path.split("/");
+    Node currNode = filesys.getRoot();
+    for (int i = 0; i < pathArr.length; i++) {
+      currNode.list.a
+      currNode = currNode.list.get(currNode.list.size()-1);
+    }
+  }
+  
+  
 
 }
