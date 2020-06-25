29d002d5a2a81bb39e751ed64fca46ad3523bf2c
ProgramTP <tirthpatelx@gmail.com>
2020-06-25 18:24:31 +0000

    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@75 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Cd.java
+++ src/driver/Cd.java
@@ -1,67 +1,73 @@
 package driver;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import sun.jvm.hotspot.runtime.ThreadStackTrace;
+public class Cd {
 
-import java.io.*;
+  ArrayList<String> cd_args;
+  FileSystem filesys;
 
-public class Cd extends DirectoryManager {
-	
-	ArrayList<String> cd_args = new ArrayList<String>();
-	FileSystem filesys = null;
-	filesys = FileSystem.getFileSys();
-	
-	public Cd(ArrayList<String> arguments) {
-		this.cd_args = arguments;
-	}
-	
-	public boolean isValid(ArrayList<String> args) {
-		String cur = args.get(0);
-		
-		if ((cur == "." || cur == ".." || cur == "/") && (args.size() == 1)){
-			return true;
-		}
-		else if (cur.matches("[A-Za-z0-9/]") && args.size() == 1) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	public void run() {
-		String argument = this.cd_args.get(0);
-		
-		if (argument == "..") {
-			this.filesys.assignCurrent(this.filesys.current.parent);
-		}
+  public Cd(String[] arguments) {
+    filesys = FileSystem.getFileSys();
+    this.cd_args = new ArrayList<String>(Arrays.asList(arguments)); 
+  }
 
-		else if (argument == "/") {
-			this.filesys.assignCurrent(this.filesys.root);
-		}
-		
-		else {
-			String[] split_args = argument.split("/");
-			for (int i = 0; i < split_args.length; i++) {
-				if (split_args[0] == this.filesys.root.name) {
-					//Make path from root
-				}
-				else {
-					//Check relative path
-				}	
-			}
-				
-		}
-	}
-	
-	
-		
-		
-	}
-	
-	
-	
-	//Access file system and then work on commands from there
-	
+  public boolean isValid() {
+    String cur = this.cd_args.get(0);
+
+    if ((cur == "." || cur == ".." || cur == "/") && (cd_args.size() == 1)) {
+      return true;
+    } else if (cur.matches("[A-Za-z0-9/]") && cd_args.size() == 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void run() {
+    String argument = this.cd_args.get(0);
+    String[] split_args = argument.split("/");
+    System.out.println(split_args);
+    if (split_args.length == 1) {
+
+      if (argument == "..") {
+        System.out.println("Retard");
+        if (!filesys.equals(null)) {
+          filesys.assignCurrent(this.filesys.current.parent);
+        }
+      }
+
+      else if (argument == "/") {
+        this.filesys.assignCurrent(this.filesys.root);
+      }
+    }
+
+    else {
+      for (int i = 0; i < split_args.length; i++) {
+        if (split_args[0] == this.filesys.root.name) {
+          // Make path from root
+        } else {
+          // Check relative path
+        }
+      }
+    }
+  }
+  
+  public static void main(String[] args) {
+    String[] sponge = new String[1];
+    sponge[0] = "..";
+    Cd test = new Cd(sponge);
+    if (test.isValid()) {
+      test.run();
+    }
+  }
+  
+
 }
+
+
+
+// Access file system and then work on commands from there
+
+
