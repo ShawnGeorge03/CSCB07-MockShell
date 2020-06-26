8c59f9d18335266e8b93c1192028287adcd2fcc8
ProgramTP <tirthpatelx@gmail.com>
2020-06-26 14:59:09 +0000

    Finished command.java
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@83 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Command.java
+++ src/driver/Command.java
@@ -1,29 +1,57 @@
 package driver;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Command {
 
   String usercommand;
   ArrayList<String> arguments = new ArrayList<String>();
   String[] splitInput;
-  
+  String command;
+
   public Command(String parsedInput) {
-	  this.splitInput = parsedInput.split(" ");
-  }
-  
-  public void run(String command, String[] arguments) {
+    this.splitInput = parsedInput.split(" ");
+    this.command = splitInput[0];
+    String args[] = Arrays.copyOfRange(splitInput, 1, splitInput.length);
+    run(command, args);
     
   }
-  
-  public static void main(String[] args) {
-	  Command new_command = new Command();
-	  new_command.usercommand = splitInput[0];
-	  for (int i = 1; i < new_command.splitInput.length; i++) {
-		  new_command.arguments.add(new_command.splitInput[i]);
-	  }
-	  
+
+  public void run(String command, String[] arguments) {
+    switch(command) {
+      case "cd":
+        Cd cd_exe = new Cd(arguments);
+        cd_exe.run();
+        break;
+      case "pwd":
+        break;
+      case "ls":
+        break;
+      case "mkdir":
+        break;
+      case "popd":
+        break;
+      case "pushd":
+        break;
+      case "cat":
+        break;
+      case "man":
+        break;
+      case "speak":
+        break;
+      
+    }
+    
   }
-  
+
+//  public static void main(String[] args) {
+//    String input = "cd ..";
+//    Command new_command = new Command(input);
+//    new_command.usercommand = new_command.splitInput[0];
+//    for (int i = 1; i < new_command.splitInput.length; i++) {
+//      new_command.arguments.add(new_command.splitInput[i]);
+//    }
+//  }
 
 }
