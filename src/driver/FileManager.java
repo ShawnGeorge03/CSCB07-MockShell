8c8572ae93aace60520b6a36fd18feb8c1845297
PatelAbhay <patel.abhay1@hotmail.com>
2020-06-26 20:31:57 +0000

    Added the FileManager class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@97 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

---
+++ src/driver/FileManager.java
@@ -0,0 +1,29 @@
+package driver;
+
+public class FileManager {
+  
+  public FileSystem filesys;
+  
+  public FileManager() {
+    filesys = FileSystem.getFileSys();
+  }
+  
+  public Node find_file(String fileName) {
+    if(!filesys.getCurrent().name.equals("C")) {
+      Node parent = filesys.getCurrent().parent;
+      for(int i = 0; i < parent.list.size(); i++) {
+        if(parent.list.get(i).name.equals(fileName)) 
+          return parent.list.get(i);
+      } 
+    }
+    else {
+      Node current = filesys.getCurrent();
+      for(int i = 0; i < current.list.size(); i++) {
+        if(current.list.get(i).name.equals(fileName) && !current.list.get(i).isDir) 
+          return current.list.get(i);
+      }
+    }
+    
+    return null;
+  }
+}
