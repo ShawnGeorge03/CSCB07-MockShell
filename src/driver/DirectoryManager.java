5d1ab0d51839887c03b1ec795026e3cb03397e11
ProgramTP <tirthpatelx@gmail.com>
2020-06-26 15:52:25 +0000

    getCurrentPath() is up and running!
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@86 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/DirectoryManager.java
+++ src/driver/DirectoryManager.java
@@ -12,7 +12,7 @@ public class DirectoryManager {
   }
   
   public String getCurrentPath() {
-    String output = "/";
+    String output = "";
     Node curr = filesys.getCurrent();
     if (curr.name == "/") {
       return "/";
@@ -22,23 +22,53 @@ public class DirectoryManager {
       curr = curr.parent;
       path.add(curr.name);
     }
-    
     int i = path.size();
     while (i > 0) {
       output = output.concat(path.get(i-1) + "/");
       i--;
     }
-
+    path.clear();
     return output.substring(0,output.length()-1);
   }
   
-  public Node makePath(String path) {
+  public int checkDirExists(ArrayList<Node> toCheck, String dir) {
+    for (int i = 0; i < toCheck.size(); i++) {
+      if ((toCheck.get(i)).name == dir); {
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+  public void makePathFromRoot(String path) {
     String[] pathArr = path.split("/");
     Node currNode = filesys.getRoot();
     for (int i = 0; i < pathArr.length; i++) {
-      currNode.list.a
-      currNode = currNode.list.get(currNode.list.size()-1);
+      int indexOfDir = checkDirExists(currNode.list, pathArr[i])
+      if (indexOfDir != -1) {
+        currNode = currNode.list.get(indexOfDir);
+      }
+      else {
+        //Error msg (path not found)
+      }
     }
+    filesys.assignCurrent(currNode);
+    //return filesys.getCurrent();
+  }
+  
+  public void makeRelativePath(String path) {
+    String[] pathArr = path.split("/");
+    Node currNode = filesys.getCurrent();
+    for (int i = 0; i < pathArr.length; i++) {
+      int indexOfDir = checkDirExists(currNode.list, pathArr[i])
+      if (indexOfDir != -1) {
+        currNode = currNode.list.get(indexOfDir);
+      }
+      else {
+        //Error msg (path not found)
+      }
+    }
+    filesys.assignCurrent(currNode);
   }
   
   
