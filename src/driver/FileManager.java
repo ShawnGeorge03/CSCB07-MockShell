79a7070923e441db30fa54a8e9a6c7d934b7b490
PatelAbhay <patel.abhay1@hotmail.com>
2020-06-27 15:31:29 +0000

    Fixed bug in the FileManager class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@111 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/FileManager.java
+++ src/driver/FileManager.java
@@ -9,7 +9,7 @@ public class FileManager {
   }
   
   public Node find_file(String fileName) {
-    if(!filesys.getCurrent().name.equals("C")) {
+    if(filesys.getCurrent().parent == null) {
       Node parent = filesys.getCurrent().parent;
       for(int i = 0; i < parent.list.size(); i++) {
         if(parent.list.get(i).name.equals(fileName)) 
