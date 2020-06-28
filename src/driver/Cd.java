f6705de07f529d09be4d6d3e2194ca2ca9a95ad5
ProgramTP <tirthpatelx@gmail.com>
2020-06-28 17:38:33 +0000

    more  functionality for cd
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@126 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Cd.java
+++ src/driver/Cd.java
@@ -3,71 +3,79 @@ package driver;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Cd {
+public class Cd extends DirectoryManager {
 
-  ArrayList<String> cd_args;
-  FileSystem filesys;
+	ArrayList<String> cd_args;
+	FileSystem filesys;
 
-  public Cd(String[] arguments) {
-    filesys = FileSystem.getFileSys();
-    this.cd_args = new ArrayList<String>(Arrays.asList(arguments)); 
-  }
+	public Cd(String[] arguments) {
+		filesys = FileSystem.getFileSys();
+		this.cd_args = new ArrayList<String>(Arrays.asList(arguments));
+	}
 
-  public boolean isValid() {
-    String cur = this.cd_args.get(0);
+	public boolean isValid() {
+		String cur = this.cd_args.get(0);
+		return true;
+//		if ((cur == "." || cur == ".." || cur == "/") && (cd_args.size() == 1)) {
+//			return true;
+//		} else if (cur.matches("[A-Za-z0-9/]") && cd_args.size() == 1) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+	}
+	public void printArr(String[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 
-    if ((cur == "." || cur == ".." || cur == "/") && (cd_args.size() == 1)) {
-      return true;
-    } else if (cur.matches("[A-Za-z0-9/]") && cd_args.size() == 1) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+	public void run() {
+		String argument = this.cd_args.get(0);
+		if (argument.equals("/")) {
+			this.filesys.assignCurrent(this.filesys.root);
+			return;
+		}
+		String[] split_args = argument.split("/");
+		
+		if (split_args.length == 1) {
+			if (argument.equals("..")) {
+				if (filesys.getCurrent().name == "/") {
+					return;
+				}
+				filesys.assignCurrent(this.filesys.current.parent);
+			}
+			
+			else if (argument.equals(".")) {
+				return;
+			}
+			
+			else {
+				this.makeRelativePath(argument);
+				System.out.println(filesys.current.name);
+			}
+		}
 
-  public void run() {
-    String argument = this.cd_args.get(0);
-    String[] split_args = argument.split("/");
-    System.out.println(split_args);
-    if (split_args.length == 1) {
+		else {
+			if (split_args[0].equals(filesys.getRoot().name)) {
+				this.makePathFromRoot(argument);
+			}
+			else {
+				this.makeRelativePath(argument);
+			}
+		}
+		System.out.println("PWD: " + this.getCurrentPath());
+		return;
+	}
 
-      if (argument == "..") {
-        System.out.println("Retard");
-        if (!filesys.equals(null)) {
-          filesys.assignCurrent(this.filesys.current.parent);
-        }
-      }
-
-      else if (argument == "/") {
-        this.filesys.assignCurrent(this.filesys.root);
-      }
-    }
-
-    else {
-      for (int i = 0; i < split_args.length; i++) {
-        if (split_args[0] == this.filesys.root.name) {
-          // Make path from root
-        } else {
-          // Check relative path
-        }
-      }
-    }
-  }
-  
-  public static void main(String[] args) {
-    String[] sponge = new String[1];
-    sponge[0] = "..";
-    Cd test = new Cd(sponge);
-    if (test.isValid()) {
-      test.run();
-    }
-  }
-  
+	public static void main(String[] args) {
+		String[] sponge = new String[1];
+		sponge[0] = "/";
+		Cd test = new Cd(sponge);
+		if (test.isValid()) {
+			test.run();
+		}
+		System.out.println(test.getCurrentPath());
+	}
 
 }
-
-
-
-// Access file system and then work on commands from there
-
-
