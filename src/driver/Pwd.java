69b20b4fc8cd53972dd4df2bf971b4c4e88d29de
ProgramTP <tirthpatelx@gmail.com>
2020-06-28 17:50:42 +0000

    Finished pwd
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@127 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Pwd.java
+++ src/driver/Pwd.java
@@ -1,30 +1,8 @@
 package driver;
 
-import java.util.ArrayList;
-
 public class Pwd extends DirectoryManager{
 	
 	public void printDirectory() {
-		Node current = filesys.getCurrent();
-		
-		ArrayList<String> workingDirectory = null;
-		
-		if (current.name == "/") {
-			System.out.println(current.name);
-			return;
-		}
-		
-		while(current.name != "/") {
-			workingDirectory.add(current.name);
-			current = current.parent;
-		}
-		
-		String output = "";
-		
-		for (int i = workingDirectory.size(); i > -1; i--) {
-			output += workingDirectory.get(i) + "/";
-		}
-		
-		System.out.println(output);
+		System.out.println(this.getCurrentPath());
 	}
 }
