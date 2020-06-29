c7869313e1cda851ba0430af9e8473aebaa7c808
ProgramTP <tirthpatelx@gmail.com>
2020-06-29 18:06:14 +0000

    bug fix
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@147 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Pwd.java
+++ src/driver/Pwd.java
@@ -1,8 +1,11 @@
 package driver;
 
-public class Pwd extends DirectoryManager{
-	
-	public void printDirectory() {
-		System.out.println(this.getCurrentPath());
-	}
+public class Pwd extends DirectoryManager {
+  
+  FileSystem filesys = null;
+
+  public void printDirectory() {
+    filesys = FileSystem.getFileSys();
+    System.out.println(this.getCurrentPath());
+  }
 }
