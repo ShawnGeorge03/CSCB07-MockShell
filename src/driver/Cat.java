6a7c29e5254c7791527c182f138bd202578ab9b2
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-06-30 03:18:20 +0000

    Added the ability to handle absolute paths
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@159 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Cat.java
+++ src/driver/Cat.java
@@ -3,19 +3,30 @@
 public class Cat extends FileManager {
   
   FileSystem filesys;
+  DirectoryManager manager;
   
   public Cat() {
     this.filesys = FileSystem.getFileSys();
+    this.manager = new DirectoryManager();
+
   }
   
-  public void readFile(String[] filePaths) {
+  public void readFile(String[] filePaths) {   
     for(int i = 0; i < filePaths.length; i++) {
-      Node file = find_file(filePaths[i]);
+      Node file = findFileGivenRelative(formatFilePath(filePaths[i]));
       if(file != null) {
         System.out.println(file.content);
       }else{
         System.out.println("File Not Found");
       }
     }
+  }
+  
+  public String formatFilePath(String filePaths) {
+    if(!filePaths.startsWith("C/")) {
+       return filePaths;
+    }else {
+      return filePaths.substring(filePaths.lastIndexOf("/") + 1);
+    }   
   }
 }
