6d2803c5ce1f1b8246ca16cd9c21817b7f440d77
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-01 20:09:16 +0000

    Fixed another bug involving the absolute path file locations
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@175 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/FileManager.java
+++ src/driver/FileManager.java
@@ -13,9 +13,21 @@ public class FileManager {
     if(!fileName.contains(".txt"))
       fileName += ".txt";
     
+    Node current = filesys.getCurrent();
+    return checkList(current, fileName);
+    
+    /*for(int i = 0; i < current.list.size(); i++) {
+      if(current.list.get(i).name.equals(fileName) && !current.list.get(i).isDir) 
+        return current.list.get(i);
+    }
+    return null;
+    */
+    /* PLEASE LEAVE ON 
     if(filesys.getCurrent().parent != null) {
       Node parent = filesys.getCurrent().parent;
+      System.out.println(parent.name);
       for(int i = 0; i < parent.list.size(); i++) {
+        //System.out.println(parent.list.get(i).name);
         if(parent.list.get(i).name.equals(fileName)) 
           return parent.list.get(i);
       } 
@@ -26,6 +38,13 @@ public class FileManager {
         if(current.list.get(i).name.equals(fileName) && !current.list.get(i).isDir) 
           return current.list.get(i);
       }
+    }*/
+  }
+  
+  private Node checkList(Node current, String fileName) {
+    for(int i = 0; i < current.list.size(); i++) {
+      if(current.list.get(i).name.equals(fileName) && !current.list.get(i).isDir) 
+        return current.list.get(i);
     }
     return null;
   }
@@ -50,20 +69,30 @@ public class FileManager {
   
   public Node findFileGivenAbsolute(String absolutePath) {
     Node current = filesys.getRoot();
-    String[] directories = absolutePath.split("/");
+    String[] directories = absolutePath.split("/");    
     
     for(int i = 1; i < directories.length; i++) {
-      if(directories[i].contains(".txt")) 
-        if((i+1) >= directories.length)
-          return findFileGivenRelative(directories[i]);
+      if(directories[i].contains(".txt")){ 
+        if((i+1) >= directories.length) {
+          return checkList(current, directories[i]);
+          /*Node found = null;
+          for(int j = 0; j < current.list.size(); j++) {
+            if(current.list.get(j).name.equals(directories[i]) && !current.list.get(j).isDir) {
+              found = current.list.get(j);
+              break;
+            }
+          }
+          return found;*/
+        }
         else System.out.println("Error -> File Cannot Contain More Files");
+      }
       else {
         Node temp = findInDirectory(directories[i]);
         if(temp != null) current = temp;
         else return null;
       }
     }
-    return null;
+    return current;
   }
   
   public boolean isValidFileName(String fileName) {
