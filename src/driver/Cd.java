8baf4c88918a385c4affe08887462719810b0469
ProgramTP <tirthpatelx@gmail.com>
2020-07-01 18:57:54 +0000

    progress with pattern
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@173 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Cd.java
+++ src/driver/Cd.java
@@ -2,71 +2,77 @@ package driver;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.*;
 
 public class Cd extends DirectoryManager {
 
-	ArrayList<String> cd_args;
-	FileSystem filesys;
-	boolean successfulPath = false;
+  ArrayList<String> cd_args;
+  FileSystem filesys;
+  boolean successfulPath = false;
 
-	public Cd(String[] arguments) {
-		filesys = FileSystem.getFileSys();
-		this.cd_args = new ArrayList<String>(Arrays.asList(arguments));
-	}
+  public Cd(String[] arguments) {
+    filesys = FileSystem.getFileSys();
+    this.cd_args = new ArrayList<String>(Arrays.asList(arguments));
+  }
 
-	public boolean isValid() {
-		String cur = this.cd_args.get(0);
-		return true;
-//		if ((cur == "." || cur == ".." || cur == "/") && (cd_args.size() == 1)) {
-//			return true;
-//		} else if (cur.matches("[A-Za-z0-9/]") && cd_args.size() == 1) {
-//			return true;
-//		} else {
-//			return false;
-//		}
-	}
-	public void printArr(String[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
-	}
+  public boolean isBackwards() {
+    String cur = this.cd_args.get(0);
+    if (Pattern.matches("[../]+", cur)) {
+      System.out.println("Ello");
+      return true;
+    }
+    return false;
+  }
 
-	public boolean run() {
-		String argument = this.cd_args.get(0);
-		if (argument.equals(this.filesys.getRoot().name)) {
-			this.filesys.assignCurrent(this.filesys.getRoot());
-			return true;
-		}
-		String[] split_args = argument.split("/");
-		
-		if (split_args.length == 1) {
-			if (argument.equals("..")) {
-				if (filesys.getCurrent().name.equals(this.filesys.getRoot().name)) {
-					return true;
-				}
-				filesys.assignCurrent(this.filesys.current.parent);
-			}
-			
-			else if (argument.equals(".")) {
-			  System.out.println("PWD: " + this.getCurrentPath());
-				return true;
-			}
-			
-			else {
-				this.makeRelativePath(argument);
-				System.out.println(filesys.current.name);
-			}
-		}
+  public void printArr(String[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
 
-		else {
-			if (split_args[0].equals(filesys.getRoot().name)) {
-				successfulPath = this.makePathFromRoot(argument);
-			}
-			else {
-				successfulPath = this.makeRelativePath(argument);
-			}
-		}
-		System.out.println("PWD: " + this.getCurrentPath());
-		return successfulPath;
-	}
+  public boolean run() {
+    String argument = this.cd_args.get(0);
+    if (argument.equals(this.filesys.getRoot().name) || (argument.charAt(0) == ('/'))) {
+      this.filesys.assignCurrent(this.filesys.getRoot());
+      return true;
+    }
+    String[] split_args = argument.split("/");
+    if (this.isBackwards()) {
+      for (int i = 0; i < split_args.length; i++) {
+        if (filesys.getCurrent().parent != null) {
+          filesys.assignCurrent(this.filesys.getCurrent().parent);
+        }
+      }
+      return true;
+    }
+
+    if (split_args.length == 1) {
+      if (argument.equals("..")) {
+        if (filesys.getCurrent().name.equals(this.filesys.getRoot().name)) {
+          return true;
+        }
+        filesys.assignCurrent(this.filesys.getCurrent().parent);
+      }
+
+      else if (argument.equals(".")) {
+        System.out.println("PWD: " + this.getCurrentPath());
+        return true;
+      }
+
+      else {
+        this.makeRelativePath(argument);
+        System.out.println(filesys.current.name);
+      }
+    }
+
+    else {
+      if (split_args[0].equals(filesys.getRoot().name)) {
+        successfulPath = this.makePathFromRoot(argument);
+      } else {
+        successfulPath = this.makeRelativePath(argument);
+      }
+    }
+    System.out.println("PWD: " + this.getCurrentPath());
+    return successfulPath;
+  }
 }
