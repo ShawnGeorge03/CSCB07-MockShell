de69a839da135c0ff8c04160eb5bc28b65b9c2cf
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-04 20:45:30 +0000

    Added the manual for cat, speak and history and fixed the error in printing the manual
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@188 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/Man.java
+++ src/driver/Man.java
@@ -1,49 +1,76 @@
 package driver;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Hashtable;
 
 public class Man extends Command{
-	Hashtable<String, String> my_dict = new Hashtable<String, String>();
+	Hashtable<String, String> my_dict;
 	
-	ArrayList<String> args;
+	public Man() {
+	  my_dict = new Hashtable<String, String>();
+      setDictionary();
+	}
 	
-	public Man(String[] args) {
-		this.args = new ArrayList<String>(Arrays.asList(args)); 
+	public void printDocumentation(String[] userInput){  
+	  if(userInput.length == 0 || userInput.length > 1 
+	      || !my_dict.containsKey(userInput[0])){
+	    System.out.println("Invalid Argument(s)");
+	  }else {
+	    System.out.println(my_dict.get(userInput[0]));
+	  }
 	}
 	
 	public void setDictionary() {
-		my_dict.put("mkdir", "");
-		my_dict.put("cat", "");
-		my_dict.put("cd", "");
-		my_dict.put("echo", "");
-		my_dict.put("history", "");
-		my_dict.put("ls", "");
-		my_dict.put("pushd", "");
-		my_dict.put("popd", "");
-		my_dict.put("pwd", "");
-		my_dict.put("speak", "");
-	}
-	
-	public void printDocumentation(){
-		setDictionary();
-		
-		if (args.size() == 0) {
-			//Error Class
-			return;
-		}
-		
-		for (int i = 0; i < args.size(); i++) {
-			if (my_dict.containsKey(args.get(i))) {
-				System.out.println(args.get(i));
-				System.out.println(my_dict.get(args.get(i)));
-			} else {
-				System.out.println(args.get(i));
-				System.out.println("That command does not exist in this shell!");
-			}
-		}
-	}
-	
-	
+	  
+      my_dict.put("speak", "Command : speak"
+          + "\n\tConverts text to audiable speech"
+          + "\n\n\tParameter : Text must be inside of quotes(\" \") "
+          + "\n\tto be converted to audio "
+          + "\n\n\tParameter : If there was no text provided then all"
+          + "\n\tinput provided next will be converted to audio, it"
+          + "\n\twill only exit if the special key word QUIT is used"
+          + "\n\n\tSample Output : $ speak \"Hello World\""
+          + "\n\tConverts Hello World to audio and exits"
+          + "\n\n\tSample Output : $ speak"
+          + "\n\t$ What's up"
+          + "\n\tConverts Whats up to audio"
+          + "\n\t$ Hello Professor, How are you QUIT"
+          + "\n\tConverts Hello Professor How are you to audio and exits ");
+      my_dict.put("mkdir", "2");
+      my_dict.put("cd", "3");
+      my_dict.put("ls", "4");
+      my_dict.put("pwd", "5");
+      my_dict.put("pushd", "6");
+      my_dict.put("popd", "7");
+      my_dict.put("history", "Command : history"
+          + "\n\tPrints out recent commands, one command per line. "
+          + "\n\tIf there is a number after the commad, then the output is "
+          + "\n\ttruncated by the specified numeber"
+          + "\n\n\tParameter : None or a whole number (3, 7, 100 and such)"
+          + "\n\n\tSample Output : $ history"
+          + "\n\t 1. ls"
+          + "\n\t 2. cd users "
+          + "\n\t 3. pwd"
+          + "\n\t 4. echo \"Hello World\" > C/pics/picturefile"
+          + "\n\t 5. cat C/pics/picturefile.txt"
+          + "\n\t 6. history"
+          + "\n\n\tSample Output : $ history 4"
+          + "\n\t 1. echo \"Hello World\" > C/pics/picturefile"
+          + "\n\t 2. cat C/pics/picturefile.txt"
+          + "\n\t 3. history"
+          + "\n\t 4. history 4");
+      my_dict.put("cat", "Command : cat"
+          + "\n\tDisplays the content of File(s) in series"
+          + "\n\n\tParameter : FILE(s) can be absolute or relative path(s)"
+          + "\n\tParameter : It requires a file or can take a list of "
+          + "\n\tFILE(s) seperated by a space"
+          + "\n\n\tSample Output : $ cat picturefile"
+          + "\n\tThis is a picture file"
+          + "\n\n\tSample Output : $ cat C/pics/picturefile text C/hello"
+          + "\n\tThis is a picture file"
+          + "\n\n\n\tHello World"
+          + "\n\n\n\tHow are you QUIT");
+      my_dict.put("echo", "10");
+      my_dict.put("man", "11");
+      my_dict.put("exit", "12");     
+  }
 }
