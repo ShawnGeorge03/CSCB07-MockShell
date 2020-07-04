f64dd1f4ea4ffc94123d2ed14740912507756b3b
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-04 04:35:52 +0000

    Possibly fixed the bug in speak, requires testing
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@181 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/TextSpeech.java
+++ src/driver/TextSpeech.java
@@ -6,62 +6,52 @@ import com.sun.speech.freetts.VoiceManager;
 
 public class TextSpeech {
    
-  Voice voice;
-  VoiceManager vm;
+  private Voice voice;
+  private VoiceManager vm;
   
   private static final String VOICENAME = "kevin16";  
   private static final String PARENT_DIR = "freetts.voices";
   private static final String VOICE_DIR = 
       "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory";
-    
+  
+  private String text;
+      
   public TextSpeech(){
     System.setProperty(PARENT_DIR, VOICE_DIR);
     this.vm = VoiceManager.getInstance();
     this.voice = this.vm.getVoice(VOICENAME);
     this.voice.allocate();
+    this.text = "";
   }
   
-  public void speak(String[] input) {
-    
-    //Creates the text to be said
-    String textToSay = Arrays.toString(input);
-    textToSay = textToSay.substring(1, textToSay.length() - 1).replace(",", "").trim();
-    
-    //Only when no arguments where provided
-    if(textToSay.length() == 0) return;
-    
-    
-    //Checks if it starts with Quotes
-    if(textToSay.startsWith("\"") && textToSay.endsWith("\"")) {
-      textToSay = textToSay.substring(textToSay.indexOf("\"") + 1, textToSay.lastIndexOf("\""));
-    }else {
-      System.out.println("Error -> Invalid Argument(s)");
-      return;
+  public void convertTextToSpeech(String[] textToSay, boolean speakMode) {  
+      
+    text = Arrays.toString(textToSay);
+    text = text.substring(1, text.length() - 1).replace(",", "").trim();
+        
+    if(text.endsWith("QUIT")) {
+      text = text.substring(0, text.indexOf("QUIT")).trim();
     }
-    
-    //Check if the string is malformed
-    if(textToSay.contains("\"")) {
-      System.out.println("Error > Malformed Argument(s)");
-      return;
-    }
-    
-    //If the user chooses to exit the speak module
-    if(textToSay.contains("QUIT")){
-      textToSay = textToSay.substring(0, textToSay.indexOf("QUIT"));
-    }
-    
-    //Says it
-    voice.speak(textToSay);
-  }  
-  
-  public boolean containsQUIT(String[] input) {
-    for(String word: input) {
-      word = word.replace("\"", "");
-      if(word.contentEquals("QUIT")) {
-        return true;
+        
+    if(!speakMode) {
+      if(text.startsWith("\"") && text.endsWith("\"")) {
+        if(!(text.length() <= 1))
+          text = text.substring(1, text.lastIndexOf("\""));
+      }else {
+        System.out.println("Missing Quotation : " + textToSay);
+        return;
       }
+      
+      if(text.indexOf("\"") != -1) {
+        System.out.println("Malformed Text : " + textToSay);
+        return;
+      } 
     }
-    return false;
+    
+    try {
+      voice.speak(text);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }   
   }
-  
 }
