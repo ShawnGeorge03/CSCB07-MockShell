223a9fe3ee2c11c10c4b809080b0e43b29667aa0
ProgramTP <tirthpatelx@gmail.com>
2020-07-05 19:56:58 +0000

    Added some errorcodes
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@200 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/ErrorHandler.java
+++ src/driver/ErrorHandler.java
@@ -7,6 +7,12 @@ public class ErrorHandler {
   HashMap<String, String> errorHash = new HashMap<String, String>();
   
   public ErrorHandler(String error_code) {
+    errorHash.put("Invalid Command", "Error: Invalid Command");
+    errorHash.put("Invalid Argument", "Error: Invalid Argument");
+    errorHash.put("Invalid File", "Error: Invalid File");
+    errorHash.put("Invalid Directory", "Error: Invalid Directory");
+    errorHash.put("File Not Found", "Error: File Not Found");
+    errorHash.put("Directory Not Found", "Error: Directory Not Found");
     System.out.println(errorHash.get(error_code));
   }
   
