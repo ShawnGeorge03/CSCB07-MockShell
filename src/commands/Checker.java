b15a0d8b39c9060cfa1c721da6b6bd3002c928b8
ProgramTP <tirthpatelx@gmail.com>
2020-07-08 15:09:23 +0000

    Added javadoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@265 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Checker.java
+++ src/commands/Checker.java
@@ -1,16 +1,35 @@
 package commands;
 
+/**
+ * Class Checker is responsible for checking if the user gave valid input and splits up the command
+ * from the arguments
+ */
 public class Checker {
   
+  /**
+   * Declare instance of CommandHandler so this can send command and arguments to it
+   */
   private CommandHandler manager;
   
+  /**
+   * Declare instance of String to contain the user's input
+   */
   public String userInput;
   
+  /**
+   * Constructor for Checker which initializes the instance variables
+   */
   public Checker() {
     this.userInput = "";
     this.manager = new CommandHandler();
   }
   
+  /**
+   * Parses the raw input that the user gives into JShell, which then sends it to CommandHandler if
+   * valid
+   * 
+   * @param input  the raw input provided by the user in JShell
+   */
   public void parseInput(String input) {
     String parsedInput = "";
     String commandName = "";
