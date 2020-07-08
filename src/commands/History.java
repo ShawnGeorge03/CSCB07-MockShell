cb5320ee3f39bacdee7bf373b5a7b94bb521730d
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-08 23:43:24 +0000

    Finished Commenting History
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@339 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/History.java
+++ src/commands/History.java
@@ -11,7 +11,7 @@ public class History implements CommandI {
   /**
    * Declare instance variable of ErrorHandler to handle error messages
    */
-  private ErrorHandler err;
+  private ErrorHandler error;
   /**
    * Declare instance variable of String to contain the output that the command may return
    */
@@ -21,7 +21,9 @@ public class History implements CommandI {
    * Constructor for History that initializes instance variables
    */
   public History() {
-    this.err = new ErrorHandler();
+    //Initializes a ErrorHandler Object
+    this.error = new ErrorHandler();
+    //Initializes a String object called output
     this.output = "";
   }
   
@@ -30,46 +32,80 @@ public class History implements CommandI {
    * 
    * @param args  the string array that contains given arguments
    * @param fullInput  the string that contains the raw input given to JShell
+   * @param val stores a boolean value
    * @return the error message if there is any or the actual history
    */
-  @Override
   public String run(String[] args, String fullInput, boolean val) {   
+    //If the user provides no arguments
     if(args.length == 0) {
+      //Calls the following methods to print the entire history
       printLastXCommands(getCommandLogSize());
+    //If the user provided 1 argument  
     }else if(args.length == 1) {
+      //Created and initialized an integer
       int number = 0;
+      //Created and initialized a boolean
       boolean numeric = true;
       
+      //Converts the given input from the user to an integer
       try {
+        //Stores the converted value if it was possible
         number = Integer.valueOf(args[0]);
+      //If the user provided something other than an Integer  
       } catch (NumberFormatException e) {
+        //Sets the boolean to false
         numeric = false;
       }
       
+      //If the input is an number and non-negative and an integer
       if(numeric && number >= 0 && number % 1 == 0) {
-        printLastXCommands(number); 
+        //Calls the following method to get the history to a certain number of 
+        //commands the user inputed
+        printLastXCommands(number);  
       } else {
-        output = err.getError("Invalid Argument", args[0]);
+        //Sets an error of Invalid Argument to be returned to the user
+        output = error.getError("Invalid Argument", args[0] + " is not either a number or positive or an integer");
       }
+    //If the user provided multiple arguments  
     }else if(args.length > 1) {
-      String parameter = Arrays.toString(args);
-      parameter = parameter.substring(1, parameter.length() - 1).replace(",", "").trim();
-      output = err.getError("Mulptile parameters provided", parameter);
+      
+      //Collects the user input from the fullInput
+      String parameter = fullInput.substring(fullInput.indexOf("history") + 7).trim();
+      //Returns an error of Mulptile parameters provided
+      output = error.getError("Mulptile parameters provided", parameter + " , either one or no input");
     }
+    
+    //Returns the valid output for the user input
     return output.trim();
   }
   
+  /**
+   * Adds a  command to the CommandLog in FileSystem
+   * 
+   * @param command this is the command that is to be placed in the CommandLog
+   */
   public void addCommands(String Command) {
     FileSystem.getCommandLog().add(Command);
   }
   
+  /**
+   * Returns the size of the CommandLog in FileSystem
+   * 
+   * @return the size of the Arraylist Object called CommandLog
+   */
   public int getCommandLogSize() {
     return FileSystem.getCommandLog().size();
   }
   
-  public void printLastXCommands(int x) {
+  private void printLastXCommands(int x) {
+    //Reads and stores the commands from the CommandLod in FileSystem
     for(int i = getCommandLogSize() - x; i < getCommandLogSize(); i++) {
-      if(i < 0) continue;
+      //If the counter is a negative number 
+      if(i < 0) {
+        //Skips from reading the commandLog
+        continue;
+      }
+      //Collects the the commands from the CommandLog
       output += (i+1)+". " + FileSystem.getCommandLog().get(i) + "\n";
     }
   }
