2a76e0cf38357aba94a59a19e66c770a2c05810d
ProgramTP <tirthpatelx@gmail.com>
2020-07-08 16:47:06 +0000

    jdoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@282 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Exit.java
+++ src/commands/Exit.java
@@ -1,7 +1,17 @@
 package commands;
 
+/**
+ * Class Exit handles exiting the JShel
+ */
 public class Exit implements CommandI{
-
+  
+  /**
+   * Ends program
+   * 
+   * @param args  the string array with all arguments
+   * @param fullInput  the string that contains the raw input provided to JShell
+   * @return null no matter what
+   */
   @Override
   public String run(String[] args, String fullInput) {
     System.exit(0);
