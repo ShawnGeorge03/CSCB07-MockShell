c346008f62a10051f26042c82d4357e9495bb745
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:48:42 +0000

    Applied styling to Pop class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@340 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Pop.java
+++ src/commands/Pop.java
@@ -2,29 +2,52 @@ package commands;
 
 import data.FileSystem;
 
+/**
+ * Class Pop is responsible for
+ */
 public class Pop extends DirectoryManager implements CommandI {
-	private ErrorHandler error;
-	
-	public Pop() {
-	    this.error = new ErrorHandler();
-	  }
-	
-	  public String run(String[] args, String fullInput, boolean val) {
-		if (args.length != 0) {
-			return error.getError("Invalid Argument", "No arguments should be given");
-		}
-	    String output = pop();
-	    return output;
-	  }
-	
-	public String pop() {
-		FileSystem.getStack().pop();
-	    if (FileSystem.getStack().size() == 0) {
-	      return "Stack is empty";
-	    }
-	    String[] path = {FileSystem.getStack().peek()};
-	    Cd newWorkingDirectory = new Cd();
-	    newWorkingDirectory.run(path);
-	    return null;
-	  }
+  /**
+   * Declare instance variable of ErrorHandler to handle error messages
+   */
+  private ErrorHandler error;
+
+  /**
+   * Constructor for Pop that initializes the ErrorHandler
+   */
+  public Pop() {
+    this.error = new ErrorHandler();
+  }
+
+  /**
+   * Method Description
+   * 
+   * @param args the string array of all arguments
+   * @param fullInput the....
+   * @param val ....
+   * @return String ....
+   */
+  public String run(String[] args, String fullInput, boolean val) {
+    if (args.length != 0) {
+      return error.getError("Invalid Argument", "No arguments should be given");
+    }
+
+    String output = pop();
+    return output;
+  }
+
+  /**
+   * Method Description
+   * 
+   * @return String ....
+   */
+  public String pop() {
+    FileSystem.getStack().pop();
+    if (FileSystem.getStack().size() == 0) {
+      return "Stack is empty";
+    }
+    String[] path = {FileSystem.getStack().peek()};
+    Cd newWorkingDirectory = new Cd();
+    newWorkingDirectory.run(path);
+    return null;
+  }
 }
