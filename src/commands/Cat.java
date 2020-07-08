7d22051140a8b3e47a8710f02f51b7d6e815aa2a
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:01:53 +0000

    Applied the Google Coding Style Sheet to Cat
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@323 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Cat.java
+++ src/commands/Cat.java
@@ -1,23 +1,23 @@
- package commands;
- 
- import data.FileSystem;
- import data.Node;
+package commands;
+
+import data.FileSystem;
+import data.Node;
 
 /**
  * Class Cat views the contents of requested file
  */
 public class Cat extends FileManager implements CommandI {
-  
+
   /**
    * Declare instance variable of FileSystem to access the filesystem
    */
   FileSystem filesys;
-  
+
   /**
    * Declare instance variable of String to contain the output that Cat may return
    */
   String output;
-  
+
   /**
    * Constructor for Cat that initializes instance variables
    */
@@ -25,45 +25,45 @@ public class Cat extends FileManager implements CommandI {
     this.filesys = FileSystem.getFileSys();
     this.output = "";
   }
-  
+
   /**
    * Checks if arguments are valid, then sends it to readFile to read the file
    * 
-   * @param args  the string array of given arguments
-   * @param fullInput  the string that contains the raw input given to JShell
+   * @param args the string array of given arguments
+   * @param fullInput the string that contains the raw input given to JShell
    * @return the contents of file
    */
   public String run(String[] args, String fullInput, boolean val) {
-    if(args.length == 0) {
+    if (args.length == 0) {
       return getErrorHandler().getError("No parameters provided", "");
-    }else {
+    } else {
       output = "";
       readFile(args);
     }
     return output;
   }
-  
+
   /**
    * Reads the requested file and gets contents
-   *  
-   * @param filePaths  the path to requested file
+   * 
+   * @param filePaths the path to requested file
    */
-  public void readFile(String[] filePaths) {   
-   Node file = null;
-    for(int i = 0; i < filePaths.length; i++) {
-      if(filePaths[i].startsWith("C/")) {
+  public void readFile(String[] filePaths) {
+    Node file = null;
+    for (int i = 0; i < filePaths.length; i++) {
+      if (filePaths[i].startsWith("C/")) {
         file = findFileGivenAbsolute(filePaths[i]);
-      }else {
+      } else {
         file = findFileGivenRelative(filePaths[i]);
       }
-      
-      if(file != null) {
+
+      if (file != null) {
         output += filesys.getContent(file);
-      }else {
+      } else {
         output += getErrorHandler().getError("File Not Found", filePaths[i]);
       }
-      
-      if(!(filePaths.length == 1 || i == filePaths.length - 1 )) 
+
+      if (!(filePaths.length == 1 || i == filePaths.length - 1))
         output += "\n" + "\n" + "\n";
     }
   }
