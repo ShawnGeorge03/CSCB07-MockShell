56684981d2d964d6d5a1d37e2eda9b0fa921c3d1
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 15:08:43 +0000

    Changed the scope of two methods as they are helper methods
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@264 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/FileManager.java
+++ src/commands/FileManager.java
@@ -37,7 +37,7 @@ public class FileManager {
     return null;
   }
   
-  public Node checkList(Node current, String fileName) {
+  private Node checkList(Node current, String fileName) {
     for(int i = 0; i < current.getList().size(); i++) {
       if(current.getList().get(i).getName().equals(fileName) && !current.getList().get(i).isDir()) 
         return current.getList().get(i);
@@ -45,7 +45,7 @@ public class FileManager {
     return null;
   }
   
-  public Node findInDirectory(String file) {
+  private Node findInDirectory(String file) {
     if(filesys.getCurrent().getParent() != null) {
       Node parent = filesys.getCurrent().getParent();
       for(int i = 0; i < parent.getList().size(); i++) {
