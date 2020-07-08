659959d1cddc602d11ef380697a1d3e2940b0a06
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:28:31 +0000

    Applied styling to ErrorHandler class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@333 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/ErrorHandler.java
+++ src/commands/ErrorHandler.java
@@ -3,15 +3,15 @@ package commands;
 import java.util.HashMap;
 
 /**
- * Class ErrorHandler contains all possible errors 
+ * Class ErrorHandler contains all possible errors
  */
 public class ErrorHandler {
-  
+
   /**
    * Declare instance variable of HashMap to contain all the error names and error messages
    */
   HashMap<String, String> errorMap;
-  
+
   /**
    * Constructor for ErrorHandler to initialize instance variables and populate HashMap
    */
@@ -19,45 +19,36 @@ public class ErrorHandler {
     this.errorMap = new HashMap<String, String>();
     initializeErrorMap();
   }
-  
+
   /**
    * Populates the HashMap with error names and error messages
    */
-  public void initializeErrorMap(){
-    errorMap.put("Invalid Command", 
-        "Error: Invalid Command");
-    errorMap.put("Invalid Argument", 
-        "Error: Invalid Argument");
-    errorMap.put("Invalid File", 
-        "Error: Invalid File");
-    errorMap.put("Invalid Directory", 
-        "Error: Invalid Directory");
-    errorMap.put("File Not Found", 
-        "Error: File Not Found");
-    errorMap.put("Directory Not Found", 
-        "Error: Directory Not Found");
-    errorMap.put("Same Directory", 
+  public void initializeErrorMap() {
+    errorMap.put("Invalid Command", "Error: Invalid Command");
+    errorMap.put("Invalid Argument", "Error: Invalid Argument");
+    errorMap.put("Invalid File", "Error: Invalid File");
+    errorMap.put("Invalid Directory", "Error: Invalid Directory");
+    errorMap.put("File Not Found", "Error: File Not Found");
+    errorMap.put("Directory Not Found", "Error: Directory Not Found");
+    errorMap.put("Same Directory",
         "Error: Same Directory with that name already exists!");
-    errorMap.put("Missing Quotes", 
-        "Error : Missing Quotes");
-    errorMap.put("Malformed Input", 
-        "Error : Malformed Input");
-    errorMap.put("Mulptile parameters provided", 
+    errorMap.put("Missing Quotes", "Error : Missing Quotes");
+    errorMap.put("Malformed Input", "Error : Malformed Input");
+    errorMap.put("Mulptile parameters provided",
         "Error : Multiple Parameters have been provided");
-    errorMap.put("No parameters provided",
-        "Error : No parameters provided");
+    errorMap.put("No parameters provided", "Error : No parameters provided");
   }
-  
+
   /**
    * Returns error messages for requested error
    * 
-   * @param errorCode  the name of the error
-   * @param parameter  the parameter that the user provided
+   * @param errorCode the name of the error
+   * @param parameter the parameter that the user provided
    * @return the error message
    */
   public String getError(String errorCode, String parameter) {
     if (errorMap.containsKey(errorCode))
       return errorMap.get(errorCode) + " : " + parameter;
     return null;
- }
+  }
 }
