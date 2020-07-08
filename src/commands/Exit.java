2d8930769e412f4869d4bde16c4f3bb4c76fb3e6
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:29:19 +0000

    Applied styling to Exit class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@334 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Exit.java
+++ src/commands/Exit.java
@@ -3,13 +3,13 @@ package commands;
 /**
  * Class Exit handles exiting the JShel
  */
-public class Exit implements CommandI{
-  
+public class Exit implements CommandI {
+
   /**
    * Ends program
    * 
-   * @param args  the string array with all arguments
-   * @param fullInput  the string that contains the raw input provided to JShell
+   * @param args the string array with all arguments
+   * @param fullInput the string that contains the raw input provided to JShell
    * @return null no matter what
    */
   @Override
