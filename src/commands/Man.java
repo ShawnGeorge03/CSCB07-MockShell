061985dad9f306a3f2ad53ca890ed5a8ed19581e
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-08 23:18:07 +0000

    Finished Commenting Man
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@328 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Man.java
+++ src/commands/Man.java
@@ -1,6 +1,5 @@
 package commands;
 
-import java.util.Arrays;
 import java.util.Hashtable;
 
 /**
@@ -10,18 +9,21 @@ public class Man implements CommandI {
   /**
    * Declare instance variable of Hashtable
    */
-  Hashtable<String, String> my_dict;
+  Hashtable<String, String> manMap;
   /**
    * Declare instance variable of ErrorHandler to handle error messages
    */
-  private ErrorHandler err;
+  private ErrorHandler error;
 
   /**
    * Constructor for Man which initializes instance variables and fills Hashtable
    */
   public Man() {
-    my_dict = new Hashtable<String, String>();
-    this.err = new ErrorHandler();
+    //Creates a HashTable Object called manMap
+    manMap = new Hashtable<String, String>();
+    //Initializes a ErrorHandler Object
+    this.error = new ErrorHandler();
+    //Initializes the Hashtable with the keys and values
     setDictionary();
   }
 
@@ -30,25 +32,31 @@ public class Man implements CommandI {
    * 
    * @param args  the string array with all arguements provided by user
    * @param fullInput the string that contains the raw input provided by user in JShell
+   * @param val stores a boolean value
    * @return the documentation of the requested commands
    */
   public String run(String[] args, String fullInput, boolean val) {
-    if (args.length == 0 || args.length > 1 || !my_dict.containsKey(args[0])) {
+    
+    //If the user provides no command name or more than one command name or a 
+    //command name that is not supported
+    if (args.length == 0 || args.length > 1 || !manMap.containsKey(args[0])) {
       
-      String param = Arrays.toString(args);
-      param = param.substring(1,param.length()-1).replace(",", "").trim();
+      //Collects the user input from the fullInput
+      String param = fullInput.substring(fullInput.indexOf("man") + 3).trim();
           
-      return err.getError("Invalid Argument", param);
+      //Returns an eror
+      return error.getError("Invalid Argument", param + " are/is not supported comman(s)");
     }
-    return my_dict.get(args[0]);
+    //Returns the appropriate command manual from the manMap Hashtable
+    return manMap.get(args[0]);
   }
 
   /**
-   * Fills hashtable with all commands and their respective documentation
+   * Fills Hashtable with all commands and their respective documentation
    */
   public void setDictionary() {
-
-    my_dict.put("speak",
+    //Adds a key named speak and adds its manual
+    manMap.put("speak",
         "Command : speak" + "\n\tConverts text to audiable speech"
             + "\n\n\tParameter : Text must be inside of quotes(\" \") "
             + "\n\tto be converted to audio "
@@ -60,29 +68,37 @@ public class Man implements CommandI {
             + "\n\t$ What's up" + "\n\tConverts Whats up to audio"
             + "\n\t$ Hello Professor, How are you QUIT"
             + "\n\tConverts Hello Professor How are you to audio and exits ");
-
-    my_dict.put("mkdir",
+    
+    //Adds a key named mkdir and adds its manual
+    manMap.put("mkdir",
         "Command: mkdir" + "\nCreates a new directory in the current working directory"
             + "\nYou can specify a relative or absolute path where you would like the "
             + "new directory to be created");
-
-    my_dict.put("cd", "3");
-    my_dict.put("ls", "Command: ls"
+    
+    //Adds a key named cd and adds its manual
+    manMap.put("cd", "3");
+    
+    //Adds a key named ls and adds its manual
+    manMap.put("ls", "Command: ls"
         + "\nLists all the files and directories within the specified directory"
         + "\nParameters: No arguments or an absolute or relative pathname"
         + "\nIf not arguments, all files and directories within current working directory will be listed"
         + "\nIf pathname provided, all files and directories in that path will be listed");
 
-    my_dict.put("pwd", "Command: pwd" + "\nPrints the current working directory");
+    //Adds a key named pwd and adds its manual
+    manMap.put("pwd", "Command: pwd" + "\nPrints the current working directory");
 
-    my_dict.put("pushd",
+    //Adds a key named pushd and adds its manual
+    manMap.put("pushd",
         "Command: pushd" + "\nTakes a relative or absolute path as an argument and pushes it onto"
             + "\nthe current directory stack");
 
-    my_dict.put("popd", "Command: popd"
+    //Adds a key named popd and adds its manual
+    manMap.put("popd", "Command: popd"
         + "\nPops the latest directory on the directory stack into the current working directory");
 
-    my_dict.put("history",
+    //Adds a key named history and adds its manual
+    manMap.put("history",
         "Command : history" + "\n\tPrints out recent commands, one command per line. "
             + "\n\tIf there is a number after the commad, then the output is "
             + "\n\ttruncated by the specified numeber"
@@ -94,7 +110,8 @@ public class Man implements CommandI {
             + "\n\t 4. echo \"Hello World\" > C/pics/picturefile"
             + "\n\t 5. cat C/pics/picturefile.txt" + "\n\t 6. history" + "\n\t 7. history 4");
 
-    my_dict.put("cat",
+    //Adds a key named cat and adds its manual
+    manMap.put("cat",
         "Command : cat" + "\n\tDisplays the content of File(s) in series"
             + "\n\n\tParameter : FILE(s) can be absolute or relative path(s)"
             + "\n\tParameter : It requires a file or can take a list of "
@@ -103,7 +120,8 @@ public class Man implements CommandI {
             + "\n\n\tSample Output : $ cat C/pics/picturefile text C/hello"
             + "\n\tThis is a picture file" + "\n\n\n\tHello World" + "\n\n\n\tHow are you QUIT");
 
-    my_dict.put("echo", "Command: echo"
+    //Adds a key named echo and adds its manual
+    manMap.put("echo", "Command: echo"
         + "\n\tParameters: String (in quotations), and arrow or"
         + "\n\tdouble arrow followed by filename"
         + "\n\n\tIf provided without arrow and fileaname, "
@@ -115,11 +133,13 @@ public class Man implements CommandI {
         + "\n\n\tFilename can be given in the form of an absolute"
         + "\n\tpath or relative path");
 
-    my_dict.put("man",
+    //Adds a key named man and adds its manual
+    manMap.put("man",
         "Command: man" + "\nProvides documentation on all commands within THIS Java Shell"
             + "\nProvides information such as arguments and function.");
 
-    my_dict.put("exit", "Command : exit" + "\n\n\tCloses the current session and leaves the Shell"
+    //Adds a key named exit and adds its manual
+    manMap.put("exit", "Command : exit" + "\n\n\tCloses the current session and leaves the Shell"
         + "\n\n\tParameter : None");
   }
 
