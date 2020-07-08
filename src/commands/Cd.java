e1b015bde84825777a166200412f890a4ea2f15b
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:11:31 +0000

    Applied Google Styling to Cd class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@324 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Cd.java
+++ src/commands/Cd.java
@@ -10,9 +10,9 @@ import data.FileSystem;
  */
 public class Cd extends DirectoryManager implements CommandI {
 
-  ArrayList<String> cd_args;
+  ArrayList<String> cdArguments;
   boolean successfulPath = false;
-  String err_output;
+  String errorOutput;
   /**
    * Declare instance of FileSystem so we can access the filesystem
    */
@@ -35,38 +35,36 @@ public class Cd extends DirectoryManager implements CommandI {
    * Starting run method which checks if arguments were given, then passes it to another run method
    * which processes the command
    * 
-   * @param args  the string array of arguments
-   * @param fullInput  the full line of input that the user gives into JShell
+   * @param args the string array of arguments
+   * @param fullInput the full line of input that the user gives into JShell
    * @return any error messages if there are any
    */
   public String run(String[] args, String fullInput, boolean val) {
     if (args.length == 0) {
-      err_output = error.getError("No parameters provided", "");
-      return err_output;
-    }else if (args.length > 1) {
-      String 
-      err_output = error.getError("Mulptile parameters provided", 
-          fullInput.substring(fullInput.indexOf("cd") + 2).trim());      
-      return err_output;
-    }
-      
-    if(!run(args)) {
-      String 
-      err_output = error.getError("Invalid Directory", 
+      errorOutput = error.getError("No parameters provided", "");
+      return errorOutput;
+    } else if (args.length > 1) {
+      String errorOutput = error.getError("Mulptile parameters provided",
           fullInput.substring(fullInput.indexOf("cd") + 2).trim());
-      return err_output;
+      return errorOutput;
+    }
+
+    if (!run(args)) {
+      String errorOutput = error.getError("Invalid Directory",
+          fullInput.substring(fullInput.indexOf("cd") + 2).trim());
+      return errorOutput;
     }
     return null;
   }
 
   /**
-   * Checks if the argument follows the pattern of ../ in which each pair of periods dictates
-   * how many directories to go up
+   * Checks if the argument follows the pattern of ../ in which each pair of periods dictates how many
+   * directories to go up
    * 
    * @return true if argument matches pattern, false otherwise
    */
   public boolean isBackwards() {
-    String cur = this.cd_args.get(0);
+    String cur = this.cdArguments.get(0);
     if (Pattern.matches("(../)+", cur) || (Pattern.matches("(../..)+", cur))) {
       return true;
     }
@@ -76,23 +74,24 @@ public class Cd extends DirectoryManager implements CommandI {
   /**
    * Main run method that executes the performance of changing directories based on what argument is
    * given. If argument is ".", nothing happens If argument is "..", go up one directory If argument
-   * is "/", change the cd to root If argument is absolute path, check if the path exists, then
-   * change to that directory If argument is relative path, check if that path exists, then change
-   * to that directory
+   * is "/", change the cd to root If argument is absolute path, check if the path exists, then change
+   * to that directory If argument is relative path, check if that path exists, then change to that
+   * directory
    * 
-   * @param arguments  the array of arguments provided by user
+   * @param arguments the array of arguments provided by user
    * @return true if the argument was processed and the change of directory was successful
    */
   public boolean run(String[] arguments) {
-    this.cd_args = new ArrayList<String>(Arrays.asList(arguments));
-    String argument = this.cd_args.get(0);
-    if (argument.equals(this.filesys.getRoot().getName()) || (argument.charAt(0) == ('/'))) {
+    this.cdArguments = new ArrayList<String>(Arrays.asList(arguments));
+    String argument = this.cdArguments.get(0);
+    if (argument.equals(this.filesys.getRoot().getName())
+        || (argument.charAt(0) == ('/'))) {
       this.filesys.assignCurrent(this.filesys.getRoot());
       return true;
     }
-    String[] split_args = argument.split("/");
+    String[] splitArgs = argument.split("/");
     if (this.isBackwards()) {
-      for (int i = 0; i < split_args.length; i++) {
+      for (int i = 0; i < splitArgs.length; i++) {
         if (filesys.getCurrent().getParent() != null) {
           filesys.assignCurrent(this.filesys.getCurrent().getParent());
         }
@@ -100,9 +99,10 @@ public class Cd extends DirectoryManager implements CommandI {
       return true;
     }
 
-    if (split_args.length == 1) {
+    if (splitArgs.length == 1) {
       if (argument.equals("..")) {
-        if (filesys.getCurrent().getName().equals(this.filesys.getRoot().getName())) {
+        if (filesys.getCurrent().getName()
+            .equals(this.filesys.getRoot().getName())) {
           return true;
         }
         filesys.assignCurrent(this.filesys.getCurrent().getParent());
@@ -119,7 +119,7 @@ public class Cd extends DirectoryManager implements CommandI {
     }
 
     else {
-      if (split_args[0].equals(filesys.getRoot().getName())) {
+      if (splitArgs[0].equals(filesys.getRoot().getName())) {
         successfulPath = this.makePathFromRoot(argument);
       } else {
         successfulPath = this.makeRelativePath(argument);
@@ -127,17 +127,4 @@ public class Cd extends DirectoryManager implements CommandI {
     }
     return successfulPath;
   }
-
-
-  // public static void main(String[] args) {
-  // Cd test = new Cd();
-  // test.run("/");
-  // System.out.println(test.getCurrentPath());
-  // test.run(".");
-  // System.out.println(test.getCurrentPath());
-  // test.run("..");
-  // System.out.println(test.getCurrentPath());
-  //
-  //
-  // }
 }
