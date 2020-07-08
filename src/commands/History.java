f516d58836e858391b038467234fd963e737dc17
ProgramTP <tirthpatelx@gmail.com>
2020-07-08 16:51:39 +0000

    jdoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@284 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/History.java
+++ src/commands/History.java
@@ -3,16 +3,35 @@ package commands;
 import java.util.Arrays;
 import data.FileSystem;
 
+/**
+ * Class history keeps track of all inputs given to JShell
+ */
 public class History implements CommandI {
   
+  /**
+   * Declare instance variable of ErrorHandler to handle error messages
+   */
   private ErrorHandler err;
+  /**
+   * Declare instance variable of String to contain the output that the command may return
+   */
   String output;
   
+  /**
+   * Constructor for History that initializes instance variables
+   */
   public History() {
     this.err = new ErrorHandler();
     this.output = "";
   }
   
+  /**
+   * Parses input and then calls print method to print history
+   * 
+   * @param args  the string array that contains given arguments
+   * @param fullInput  the string that contains the raw input given to JShell
+   * @return the error message if there is any or the actual history
+   */
   @Override
   public String run(String[] args, String fullInput) {   
     if(args.length == 0) {
@@ -47,7 +66,7 @@ public class History implements CommandI {
   public int getCommandLogSize() {
     return FileSystem.getCommandLog().size();
   }
-    
+  
   public void printLastXCommands(int x) {
     for(int i = getCommandLogSize() - x; i < getCommandLogSize(); i++) {
       if(i < 0) continue;
