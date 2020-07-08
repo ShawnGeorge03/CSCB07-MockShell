b606e860aca70792e2cff46825c4bf0ed6ce3bfc
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:54:18 +0000

    Applied styling to Pwd class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@343 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Pwd.java
+++ src/commands/Pwd.java
@@ -6,7 +6,7 @@ import commands.DirectoryManager;
  * Class Pwd is responsible for providing the absolute path of the current working directory
  */
 public class Pwd extends DirectoryManager implements CommandI {
-  
+
   /**
    * Declare instance of ErrorHandler to handle any errors that occur
    */
@@ -16,28 +16,29 @@ public class Pwd extends DirectoryManager implements CommandI {
    * Constructor for Pwd that initializes the ErrorHandler object
    */
   public Pwd() {
-    //Initializes a ErrorHandler Object
+    // Initializes a ErrorHandler Object
     this.error = new ErrorHandler();
   }
-  
+
   /**
-   * Run method first checks to see if the user has inputted any arguments, else it will return the 
+   * Run method first checks to see if the user has inputted any arguments, else it will return the
    * absolute path to the current working directory
    * 
-   * @param args  the string array of arguments
-   * @param fullInput  the full line of input that the user gives into JShell
+   * @param args the string array of arguments
+   * @param fullInput the full line of input that the user gives into JShell
    * @param val stores a boolean value
    * @return String holding the absolute path to the current working directory, or an error message
    */
   public String run(String[] args, String fullInput, boolean val) {
-    //If the user provides any input for the following function
-      if (args.length != 0) {
-        //Returns an Invalid arguments error
-          return error.getError("Invalid Argument", "pwd doesn't take any arguments");
-      }
-      //Returns the current working directory the user is in
-      return this.getCurrentPath();
+    // If the user provides any input for the following function
+    if (args.length != 0) {
+      // Returns an Invalid arguments error
+      return error.getError("Invalid Argument",
+          "pwd doesn't take any arguments");
+    }
+    // Returns the current working directory the user is in
+    return this.getCurrentPath();
   }
-  
- 
+
+
 }
