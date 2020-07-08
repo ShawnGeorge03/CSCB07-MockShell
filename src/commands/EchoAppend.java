9313f15ffbb95d5cfaf9e164d349898a4913def3
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:26:12 +0000

    Applied styling to echo append class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@331 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/EchoAppend.java
+++ src/commands/EchoAppend.java
@@ -5,25 +5,24 @@ import data.Node;
 /**
  * Class EchoAppend appends data onto the contents of the requested file
  */
-public class EchoAppend extends Echo{
-  
+public class EchoAppend extends Echo {
+
   /**
    * Takes the new content and appends it to the contents of the file
    * 
-   * @param fileContents  the contents the user wishes to append
-   * @param fileName  the name of requested file
+   * @param fileContents the contents the user wishes to append
+   * @param fileName the name of requested file
    */
   public void execute(String fileContents, String fileName) {
-    
-    String[] path = fileName.split("/");
-    
-    if(path.length == 1) {
-      if(isValidFileName(fileName)) {
-        if(findFileGivenRelative(fileName) != null) {
+
+    String[] givenPath = fileName.split("/");
+
+    if (givenPath.length == 1) {
+      if (isValidFileName(fileName)) {
+        if (findFileGivenRelative(fileName) != null) {
           Node file = findFileGivenRelative(fileName);
           file.setContent(file.getContent() + "\n" + fileContents);
-        }
-        else {
+        } else {
           Node newFile = new Node();
           newFile.setDir(false);
           newFile.setContent(fileContents);
@@ -31,17 +30,17 @@ public class EchoAppend extends Echo{
           newFile.setParent(filesys.getCurrent());
           filesys.addToDirectory(newFile);
         }
-      }
-      else System.out.println(this.getErrorHandler().getError("Invalid File", fileName));
+      } else
+        System.out
+            .println(this.getErrorHandler().getError("Invalid File", fileName));
     }
-    
+
     else {
-      if(isValidFileName(path[path.length-1])) {
-        if(findFileGivenAbsolute(fileName) != null) {
+      if (isValidFileName(givenPath[givenPath.length - 1])) {
+        if (findFileGivenAbsolute(fileName) != null) {
           Node file = findFileGivenAbsolute(fileName);
           file.setContent(fileContents);
-        }
-        else {
+        } else {
           Node currentNode = this.filesys.getCurrent();
           String desiredPath = getCurrentPath() + fileName;
           desiredPath = desiredPath.substring(0, desiredPath.lastIndexOf("/"));
@@ -50,15 +49,16 @@ public class EchoAppend extends Echo{
           Node newFile = new Node();
           newFile.setDir(false);
           newFile.setContent(fileContents);
-          newFile.setName(path[path.length-1]);
+          newFile.setName(givenPath[givenPath.length - 1]);
           newFile.setParent(parent);
           filesys.addToDirectory(newFile);
           this.filesys.assignCurrent(currentNode);
         }
-      }
-      else System.out.println(this.getErrorHandler().getError("Invalid File", path[path.length-2]));
+      } else
+        System.out.println(this.getErrorHandler().getError("Invalid File",
+            givenPath[givenPath.length - 2]));
     }
-    
+
   }
-  
+
 }
