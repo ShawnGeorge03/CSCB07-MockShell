fc1d537cc490ea12345286c4041bf5ac0b33d154
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:12:56 +0000

    Applied Google Styling to Checker class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@325 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Checker.java
+++ src/commands/Checker.java
@@ -5,17 +5,17 @@ package commands;
  * from the arguments
  */
 public class Checker {
-  
+
   /**
    * Declare instance of CommandHandler so this can send command and arguments to it
    */
   private CommandHandler manager;
-  
+
   /**
    * Declare instance of String to contain the user's input
    */
   public String userInput;
-  
+
   /**
    * Constructor for Checker which initializes the instance variables
    */
@@ -23,30 +23,31 @@ public class Checker {
     this.userInput = "";
     this.manager = new CommandHandler();
   }
-  
+
   /**
    * Parses the raw input that the user gives into JShell, which then sends it to CommandHandler if
    * valid
    * 
-   * @param input  the raw input provided by the user in JShell
+   * @param input the raw input provided by the user in JShell
    */
   public void parseInput(String input) {
     String parsedInput = "";
     String commandName = "";
     String args = "";
-    if(input.contains("\"")) {
-      commandName = input.substring(0, input.indexOf("\"")).trim().replaceAll("\\s+", " ");
+    if (input.contains("\"")) {
+      commandName = input.substring(0, input.indexOf("\"")).trim()
+          .replaceAll("\\s+", " ");
       args = input.substring(input.indexOf("\""), input.length());
       parsedInput = commandName + " " + args;
-    }
-    else if(input.contains("'")) {
-      commandName = input.substring(0, input.indexOf("'")).trim().replaceAll("\\s+", " ");
+    } else if (input.contains("'")) {
+      commandName =
+          input.substring(0, input.indexOf("'")).trim().replaceAll("\\s+", " ");
       args = input.substring(input.indexOf("'"), input.length());
       parsedInput = commandName + " " + args;
-    }
-    else parsedInput = input.trim().replaceAll("\\s+", " ");
+    } else
+      parsedInput = input.trim().replaceAll("\\s+", " ");
 
     manager.setCommand(parsedInput);
   }
-  
+
 }
