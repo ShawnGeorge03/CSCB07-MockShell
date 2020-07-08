c80015ef11b5a8a6cd5808edb4729dbfa1306ec6
ProgramTP <tirthpatelx@gmail.com>
2020-07-08 16:15:48 +0000

    jdoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@278 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Cat.java
+++ src/commands/Cat.java
@@ -3,17 +3,36 @@
  import data.FileSystem;
  import data.Node;
 
+/**
+ * Class Cat views the contents of requested file
+ */
 public class Cat extends FileManager implements CommandI {
   
+  /**
+   * Declare instance variable of FileSystem to access the filesystem
+   */
   FileSystem filesys;
   
+  /**
+   * Declare instance variable of String to contain the output that Cat may return
+   */
   String output;
   
+  /**
+   * Constructor for Cat that initializes instance variables
+   */
   public Cat() {
     this.filesys = FileSystem.getFileSys();
     this.output = "";
   }
   
+  /**
+   * Checks if arguments are valid, then sends it to readFile to read the file
+   * 
+   * @param args  the string array of given arguments
+   * @param fullInput  the string that contains the raw input given to JShell
+   * @return the contents of file
+   */
   public String run(String[] args, String fullInput) {
     if(args.length == 0) {
       return getErrorHandler().getError("No parameters provided", "");
@@ -24,6 +43,11 @@ public class Cat extends FileManager implements CommandI {
     return output;
   }
   
+  /**
+   * Reads the requested file and gets contents
+   *  
+   * @param filePaths  the path to requested file
+   */
   public void readFile(String[] filePaths) {   
    Node file = null;
     for(int i = 0; i < filePaths.length; i++) {
