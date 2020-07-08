e74d63205079dc4e115ce7b9988189884d953efa
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 22:27:46 +0000

    Added the Javadoc for Echo class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@316 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Echo.java
+++ src/commands/Echo.java
@@ -1,11 +1,27 @@
 package commands;
 
+/**
+ * Class Echo is responsible for printing to the shell and redirecting the input to perform the 
+ * appropriate task
+ */
 public class Echo extends FileManager implements CommandI {
 
+  /** Declare instance variable of String to hold the arguments */
   private String argument;
   private int num_arrow;
+  
+  /** Declare instance variable of String to hold the output that will be returned */
   String output = "";
 
+  /**
+   * Main run method that checks if the user had inputted any arguments to the command.
+   * It splices the input so that it can send parsed data to the appropriate implementation of echo.
+   * Calls the execute method that will perform the required task for the appropriate scenario.
+   * Returns a String that will either contain an error message if there was an error or return null
+   * 
+   * @param args  the String array of arguments provided by user (split from a whitespace)
+   * @return String  will either be null if there were no errors or an appropriate error message 
+   */
   public String run(String[] args, String fullInput, boolean val) {
     if(args.length == 0) {
       return getErrorHandler().getError("No parameters provided", "");
@@ -18,7 +34,7 @@ public class Echo extends FileManager implements CommandI {
     return output;
   }
 
-  public boolean hasQuotations(String fullInput) {
+  private boolean hasQuotations(String fullInput) {
     if (fullInput.contains("\"")) {
       int num = fullInput.length() - fullInput.replaceAll("\"", "").length();
       if (num % 2 == 0)
@@ -27,11 +43,11 @@ public class Echo extends FileManager implements CommandI {
     return false;
   }
 
-  public int count_arrows(String parsedInput) {
+  private int count_arrows(String parsedInput) {
     return parsedInput.length() - parsedInput.replaceAll(">", "").length();
   }
 
-  public String fix_argument(String[] spliced_input) {
+  private String fix_argument(String[] spliced_input) {
     String args = "";
 
     for (int i = 1; i < spliced_input.length; i++) {
@@ -41,6 +57,12 @@ public class Echo extends FileManager implements CommandI {
     return args.substring(0, args.length() - 1);
   }
 
+  
+  /**
+   * Method that is responsible for printing the required arguments to the shell/console.
+   * 
+   * @param args  the string array of arguments
+   */
   public void printToConsole(String[] args) {
     for (int i = 1; i < args.length; i++) {
       output += args[i] + " ";
@@ -49,7 +71,7 @@ public class Echo extends FileManager implements CommandI {
     output = output.substring(0, output.length() - 1).replaceAll("\"", "");
   }
 
-  public void execute(String[] args, String fullInput) {
+  private void execute(String[] args, String fullInput) {
     String fileContents = "";
     if (hasQuotations(fullInput)) {
       fileContents = fullInput.substring(fullInput.indexOf("\"") + 1, fullInput.lastIndexOf("\""));
