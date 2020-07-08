f95f875c01058df86cbbbe8f5bc13a0196b10fe2
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:33:40 +0000

    Applied styling to Ls class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@336 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Ls.java
+++ src/commands/Ls.java
@@ -18,51 +18,52 @@ public class Ls extends DirectoryManager implements CommandI {
    * Declare instance variable of ErrorHandler to handle error messages
    */
   private ErrorHandler error;
-  
+
   /**
    * Constructor for Ls to initialize error
    */
   public Ls() {
     this.error = new ErrorHandler();
   }
-  
+
   /**
    * Displays all of the given directory's child nodes
    * 
-   * @param args  the string array that contains the arguments
-   * @param fullInput  the string that contains the raw input that the user provides to JShell
+   * @param args the string array that contains the arguments
+   * @param fullInput the string that contains the raw input that the user provides to JShell
    * @return null always
    */
   public String run(String[] arguments, String fullInput, boolean val) {
-	  this.args = new ArrayList<String>(Arrays.asList(arguments));
-	  String output = "";
-		
-	    if (args.size() == 0) {
-	      Node curr = filesys.getCurrent();
-	      for (int i = 0; i < curr.getList().size(); i++) {
-	    	  output += curr.getList().get(i).getName() + "\n";
-	      }
-	    } else {
-	      for (int i = 0; i < args.size(); i++) {
-	    	  String[] path = {args.get(i)};
-		      String[] currentPath = {getCurrentPath()};
+    this.args = new ArrayList<String>(Arrays.asList(arguments));
+    String output = "";
 
-		      Cd traverse = new Cd();
-		      if (traverse.run(path)) {
-		        Node current = FileSystem.getFileSys().getCurrent();
-		        System.out.println(path[0]);
-		        for (int j = 0; j < current.getList().size(); j++) {
-		          System.out.println(current.getList().get(j).getName());
-		        }
-		      } else {
-		        return error.getError("Invalid Directory", args.get(i) + " is not a valid directory");
-		      }
+    if (args.size() == 0) {
+      Node curr = filesys.getCurrent();
+      for (int i = 0; i < curr.getList().size(); i++) {
+        output += curr.getList().get(i).getName() + "\n";
+      }
+    } else {
+      for (int i = 0; i < args.size(); i++) {
+        String[] path = {args.get(i)};
+        String[] currentPath = {getCurrentPath()};
 
-		      Cd goBack = new Cd();
-		      goBack.run(currentPath);
-		      System.out.println();
-	      }
-	    }
+        Cd traverse = new Cd();
+        if (traverse.run(path)) {
+          Node current = FileSystem.getFileSys().getCurrent();
+          System.out.println(path[0]);
+          for (int j = 0; j < current.getList().size(); j++) {
+            System.out.println(current.getList().get(j).getName());
+          }
+        } else {
+          return error.getError("Invalid Directory",
+              args.get(i) + " is not a valid directory");
+        }
+
+        Cd goBack = new Cd();
+        goBack.run(currentPath);
+        System.out.println();
+      }
+    }
     return null;
   }
 }
