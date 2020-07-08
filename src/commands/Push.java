4326a09792b590ed52e5ccb22b3cf478e3617115
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 23:53:33 +0000

    Applied styling to Push class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@342 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Push.java
+++ src/commands/Push.java
@@ -5,52 +5,54 @@ import data.FileSystem;
 import data.Node;
 
 /**
- * Class Push is responsible for 
+ * Class Push is responsible for
  */
 public class Push extends DirectoryManager implements CommandI {
-  
+
   /**
    * Declare an instance of ErrorHandler to handle any errors that occur
    */
   private ErrorHandler error;
 
   /**
-   * Constructor for Pwd that initializes the ErrorHandler object
+   * Constructor for Push that initializes the ErrorHandler object
    */
   public Push() {
     this.error = new ErrorHandler();
   }
-  
+
   /**
    * Method Description
    * 
-   * @param args  the string array of arguments
-   * @param fullInput  the full line of input that the user gives into JShell
-   * @return String holding the absolute path to the current working directory, or an error message
+   * @param args the string array of arguments
+   * @param fullInput the full line of input that the user gives into JShell
+   * @return String ....
    */
   public String run(String[] args, String fullInput, boolean val) {
-	Cd goBack = new Cd();
-	String[] root = {FileSystem.getFileSys().getRoot().getName()};
-    
+    Cd goBack = new Cd();
+    String[] root = {FileSystem.getFileSys().getRoot().getName()};
+
     if (args.length != 1) {
-        return error.getError("Invalid Argument",  Integer.toString(args.length) + " arguments, expecting 1 argument");
-      }
-      
-      if (FileSystem.getStack().isEmpty()) {
-        FileSystem.getStack().push(getCurrentPath());
-      }
-      
-      if (goBack.run(args)) {
-    	  goBack.run(root);
-      }else {
-    	return error.getError("Invalid Directory", args[0] + "is not a valid directory");
-      }
-      
-      
-      FileSystem.getStack().push(args[0]);
-      Cd newWorkingDirectory = new Cd();
-      newWorkingDirectory.run(args);
-    
+      return error.getError("Invalid Argument",
+          Integer.toString(args.length) + " arguments, expecting 1 argument");
+    }
+
+    if (FileSystem.getStack().isEmpty()) {
+      FileSystem.getStack().push(getCurrentPath());
+    }
+
+    if (goBack.run(args)) {
+      goBack.run(root);
+    } else {
+      return error.getError("Invalid Directory",
+          args[0] + "is not a valid directory");
+    }
+
+
+    FileSystem.getStack().push(args[0]);
+    Cd newWorkingDirectory = new Cd();
+    newWorkingDirectory.run(args);
+
     return null;
   }
 }
