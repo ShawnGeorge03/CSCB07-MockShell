3e476d82b137f18475189449158b8651c701f184
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-08 15:20:38 +0000

    Added Javadoc to Pwd class
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@267 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Pwd.java
+++ src/commands/Pwd.java
@@ -4,12 +4,26 @@ import commands.DirectoryManager;
 
 public class Pwd extends DirectoryManager implements CommandI {
   
+  /**
+   * Declare instance of ErrorHandler
+   */
   ErrorHandler error;
 
+  /**
+   * Constructor for Pwd
+   */
   public Pwd() {
     this.error = new ErrorHandler();
   }
   
+  /**
+   * Run method first checks to see if the user has inputted any arguments, else it will return the 
+   * absolute path to the current working directory
+   * 
+   * @param args  the string array of arguments
+   * @param fullInput  the full line of input that the user gives into JShell
+   * @return String holding the absolute path to the current working directory, or an error message
+   */
   public String run(String[] args, String fullInput) {
       if (args.length != 0) {
           return error.getError("Invalid Argument", "pwd doesn't take any arguments");
