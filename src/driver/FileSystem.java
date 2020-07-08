4e85e3530cedc9100abf4cb2e670b65294c58af6
ProgramTP <tirthpatelx@gmail.com>
2020-07-08 18:48:07 +0000

    jdoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@300 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

---
+++ src/driver/FileSystem.java
@@ -0,0 +1,88 @@
+package driver;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+public class FileSystem {
+	
+	static FileSystem fileSys = null;
+	static ArrayList<String> CommandLog =  new ArrayList<String>();
+	static Deque<String> stack = new ArrayDeque<String>();
+
+	Node root = new Node();	
+	Node current = new Node();
+	
+	
+	private FileSystem() {
+		root.isDir = true;
+		root.content = null;
+		root.name = "C";
+		root.isRoot = true;
+		root.parent = null;
+		current = root;
+		
+		//First dummy in Root
+		Node users = new Node();
+		users.isDir = true;
+		users.content = null;
+		users.name = "users";
+		users.parent = root;
+		
+		//second dummy in Root
+		Node pics = new Node();
+		pics.isDir = true;
+		pics.content = null;
+		pics.name = "pics";
+		pics.parent = root;
+		
+		//Picfile in pics
+		Node picfile = new Node();
+		picfile.isDir = false;
+		picfile.content = "this is a picture";
+		picfile.name = "picturefile.txt";
+		picfile.parent = pics;
+		
+		pics.list.add(picfile);
+		
+		root.list.add(users);
+		root.list.add(pics);
+		current = picfile;
+		
+		// C
+		    // Users
+			// pics
+				//picturefile.txt
+	}
+	
+	public static FileSystem getFileSys() {
+		if (fileSys == null) {
+			fileSys = new FileSystem();
+			return fileSys;
+		} else {
+			return fileSys;
+		}
+	}
+	
+	public Node getRoot() {
+		return root;
+	}
+	
+	public Node getCurrent() {
+		return current;
+	}
+	
+	public String getContent(Node file) {
+	  return file.content;
+	}
+	
+	public void assignCurrent(Node currentDirectory) {
+		current = currentDirectory;
+	}
+	
+	public void addToDirectory(Node newNode) {
+		newNode.parent = current;
+		current.list.add(newNode);
+	}
+
+}
