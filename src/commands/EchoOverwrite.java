2f9131d92637c02d75e3101998bdaced5778fc7c
PatelAbhay <patel.abhay1@hotmail.com>
2020-07-09 02:48:56 +0000

    Added comments to EchoAppend and EchoOverwrite
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@420 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/EchoOverwrite.java
+++ src/commands/EchoOverwrite.java
@@ -14,51 +14,66 @@ public class EchoOverwrite extends Echo {
    * @param fileName name of the file
    */
   public void execute(String fileContents, String fileName) {
-
+    // Parses the user inputted path
     String[] givenPath = fileName.split("/");
-
+    // If given only the file (relative path)
     if (givenPath.length == 1) {
-      if (isValidFileName(fileName)) {
-        if (findFileGivenRelative(fileName) != null) {
-          Node file = findFileGivenRelative(fileName);
-          file.setContent(fileContents);
-        } else {
-          Node newFile = new Node();
-          newFile.setDir(false);
-          newFile.setContent(fileContents);
-          newFile.setName(fileName);
-          newFile.setParent(filesys.getCurrent());
-          filesys.addToDirectory(newFile);
-        }
-      } else
-        System.out
-            .println(this.getErrorHandler().getError("Invalid File", fileName));
+      executeRelativePath(fileContents, fileName);
     }
-
+    // If given absolute path
     else {
-      if (isValidFileName(givenPath[givenPath.length - 1])) {
-        if (findFileGivenAbsolute(fileName) != null) {
-          Node file = findFileGivenAbsolute(fileName);
-          file.setContent(fileContents);
-        } else {
-          Node currentNode = this.filesys.getCurrent();
-          String desiredPath = getCurrentPath() + fileName;
-          desiredPath = desiredPath.substring(0, desiredPath.lastIndexOf("/"));
-          Node parent = findFolderGivenAbsolute(desiredPath);
-          this.filesys.assignCurrent(parent);
-          Node newFile = new Node();
-          newFile.setDir(false);
-          newFile.setContent(fileContents);
-          newFile.setName(givenPath[givenPath.length - 1]);
-          newFile.setParent(parent);
-          filesys.addToDirectory(newFile);
-          this.filesys.assignCurrent(currentNode);
-        }
-      } else
-        System.out.println(this.getErrorHandler().getError("Invalid File",
-            givenPath[givenPath.length - 2]));
+      executeAbsolutePath(fileContents, fileName, givenPath);
     }
 
+  }
+
+  private void executeRelativePath(String fileContents, String fileName) {
+    // Check if valid fileName
+    if (isValidFileName(fileName)) {
+      // If already exists
+      if (findFileGivenRelative(fileName) != null) {
+        Node file = findFileGivenRelative(fileName);
+        file.setContent(fileContents);
+      } else {
+        // Create new file and add to FileSystem
+        Node newFile = new Node();
+        newFile.setDir(false);
+        newFile.setContent(fileContents);
+        newFile.setName(fileName);
+        newFile.setParent(filesys.getCurrent());
+        filesys.addToDirectory(newFile);
+      }
+    } else
+      System.out
+          .println(this.getErrorHandler().getError("Invalid File", fileName));
+  }
+
+  private void executeAbsolutePath(String fileContents, String fileName,
+      String[] givenPath) {
+    //Check if fileName is valid
+    if (isValidFileName(givenPath[givenPath.length - 1])) {
+   // If already exists
+      if (findFileGivenAbsolute(fileName) != null) {
+        Node file = findFileGivenAbsolute(fileName);
+        file.setContent(fileContents);
+      } else {
+        //Create new file and add to the FileSystem
+        Node currentNode = this.filesys.getCurrent();
+        String desiredPath = getCurrentPath() + fileName;
+        desiredPath = desiredPath.substring(0, desiredPath.lastIndexOf("/"));
+        Node parent = findFolderGivenAbsolute(desiredPath);
+        this.filesys.assignCurrent(parent);
+        Node newFile = new Node();
+        newFile.setDir(false);
+        newFile.setContent(fileContents);
+        newFile.setName(givenPath[givenPath.length - 1]);
+        newFile.setParent(parent);
+        filesys.addToDirectory(newFile);
+        this.filesys.assignCurrent(currentNode);
+      }
+    } else
+      System.out.println(this.getErrorHandler().getError("Invalid File",
+          givenPath[givenPath.length - 2]));
   }
 
 }
