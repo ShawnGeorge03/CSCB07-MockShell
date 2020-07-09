38efe3663f1187499714fe1d1eccbcef9572bf02
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-09 01:06:39 +0000

    Finished Commenting CommandI
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@357 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/CommandI.java
+++ src/commands/CommandI.java
@@ -1,7 +1,22 @@
 package commands;
 
+/**
+ * Interface CommandI is responsible for providing a comman method to call any 
+ * class containing that certain command
+ */  
+
 interface CommandI {
 
+ /**
+  * Runs the command based on the implementation of this method in the 
+  * respective class
+  * 
+  * @param args the string array of arguments
+  * @param fullInput the full line of input that the user gives into JShell
+  * @param val stores a boolean value
+  *
+  * @return any error message or valid output for user input
+  */  
   public String run(String args[], String fullInput, boolean val);
 
 }
