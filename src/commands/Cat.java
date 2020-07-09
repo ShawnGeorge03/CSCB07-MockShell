b22c22f4c00ec76b05f0af780d8769c262037bae
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-09 00:04:10 +0000

    Finished Commenting Cat
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@344 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Cat.java
+++ src/commands/Cat.java
@@ -14,7 +14,8 @@ public class Cat extends FileManager implements CommandI {
   FileSystem filesys;
 
   /**
-   * Declare instance variable of String to contain the output that Cat may return
+   * Declare instance variable of String to contain the output that Cat may 
+   * return
    */
   String output;
 
@@ -22,7 +23,9 @@ public class Cat extends FileManager implements CommandI {
    * Constructor for Cat that initializes instance variables
    */
   public Cat() {
+    //Get the current FileSystem
     this.filesys = FileSystem.getFileSys();
+    //Initializing the String object output
     this.output = "";
   }
 
@@ -31,42 +34,52 @@ public class Cat extends FileManager implements CommandI {
    * 
    * @param args the string array of given arguments
    * @param fullInput the string that contains the raw input given to JShell
+   * @param val stores a boolean value
    * @return the contents of file
    */
   public String run(String[] args, String fullInput, boolean val) {
     if (args.length == 0) {
+      //Returns an error of No parameters provided
       return getErrorHandler().getError("No parameters provided", "");
     } else {
+      //Initializing the String object output after each time the method is called
       output = "";
+      //Calls the readFile function to return what is in the file
       readFile(args);
     }
+    //Returns the output for the arguments
     return output;
   }
 
-  /**
-   * Reads the requested file and gets contents
-   * 
-   * @param filePaths the path to requested file
-   */
-  public void readFile(String[] filePaths) {
+  private void readFile(String[] filePaths) {
+    //Declares and initialized a Node to null
     Node file = null;
+    //Runs through all the filePaths and stores the output for each case
     for (int i = 0; i < filePaths.length; i++) {
+      //If the file path is an absolute path
       if (filePaths[i].startsWith("C/")) {
+        //Calls the following method to return a reference to that specific Node
         file = findFileGivenAbsolute(filePaths[i]);
+      //If the file path is a relative path
       } else {
+        //Calls the following method to return a reference to that specific Node
         file = findFileGivenRelative(filePaths[i]);
       }
 
+      //If the file does exist
       if (file != null) {
+        //Collect and append the text to the String object output
         output += filesys.getContent(file);
+      //If the file does not exist
       } else {
+        //Collect and append the error of File Not Found
         output += getErrorHandler().getError("File Not Found", filePaths[i]);
       }
-
+      
+      //If it is not one file or it is the last file in the filePaths
       if (!(filePaths.length == 1 || i == filePaths.length - 1))
+        //Append multiple line breaks to the String object output
         output += "\n" + "\n" + "\n";
     }
   }
-
-
 }
