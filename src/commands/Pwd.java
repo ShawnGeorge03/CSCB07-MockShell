5adcbe463e96f7ad415015c9479e3f5bcba511e8
ProgramTP <tirthpatelx@gmail.com>
2020-07-09 02:45:34 +0000

    fixed jdoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@418 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Pwd.java
+++ src/commands/Pwd.java
@@ -24,9 +24,9 @@ public class Pwd extends DirectoryManager implements CommandI {
    * Run method first checks to see if the user has inputted any arguments, else it will return the
    * absolute path to the current working directory
    * 
-   * @param args the string array of arguments
-   * @param fullInput the full line of input that the user gives into JShell
-   * @param val stores a boolean value
+   * @param args  the string array of arguments
+   * @param fullInput  the full line of input that the user gives into JShell
+   * @param val  stores a boolean value
    * @return String holding the absolute path to the current working directory, or an error message
    */
   public String run(String[] args, String fullInput, boolean val) {
