8746614a9dfd72e39967e1d3884ade13a3be5d5e
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-09 00:49:09 +0000

    Finished Commenting CommandHandler
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@356 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/CommandHandler.java
+++ src/commands/CommandHandler.java
@@ -7,23 +7,25 @@ import java.util.HashMap;
 /**
  * Class CommandHandler is responsible for creating instances of the requested command and running
  * them
+ * 
+ * Note : Part of this code was taken from Lab6 src.lab6_project.executionOfProgram.java file
  */
 public class CommandHandler {
 
   /**
    * Declare instance variable of String array to hold the command and its arguments
    */
-  String[] splitInput;
+  private String[] splitInput;
   /**
    * Declare instance variable of String to hold the actual command
    */
-  String command;
+  private String command;
   /**
    * Declare instance variable of HashMap to hold all of the commands this program provides
    */
-  HashMap<String, String> commandMap;
+  private HashMap<String, String> commandMap;
 
-  boolean speakMode = false;
+  private boolean speakMode = false;
 
   private ErrorHandler errorManager;
   /**
@@ -36,28 +38,40 @@ public class CommandHandler {
    * commandMap with commands
    */
   public CommandHandler() {
+    //Initializes a ErrorHandler Object
     errorManager = new ErrorHandler();
+    // Creates a HashTable Object called commandMap
     commandMap = new HashMap<String, String>();
+    // Initializes the HashMap with the keys and values
     intizializeCommandMap();
+    
   }
 
-  /**
-   * Populates HashMap with commands and their respective methods
-   */
-  public void intizializeCommandMap() {
+  private void intizializeCommandMap() {
+    // Adds a key named speak and adds its path
     commandMap.put("speak", "commands.TextSpeech");
+    // Adds a key named mkdir and adds its path
     commandMap.put("mkdir", "commands.Mkdir");
+    // Adds a key named cd and adds its path
     commandMap.put("cd", "commands.Cd");
+    // Adds a key named ls and adds its path
     commandMap.put("ls", "commands.Ls");
+    // Adds a key named pwd and adds its path
     commandMap.put("pwd", "commands.Pwd");
+    // Adds a key named pushd and adds its path
     commandMap.put("pushd", "commands.Push");
+    // Adds a key named popd and adds its path
     commandMap.put("popd", "commands.Pop");
+    // Adds a key named history and adds its path
     commandMap.put("history", "commands.History");
+    // Adds a key named cat and adds its path
     commandMap.put("cat", "commands.Cat");
+    // Adds a key named echo and adds its path
     commandMap.put("echo", "commands.Echo");
+    // Adds a key named man and adds its path
     commandMap.put("man", "commands.Man");
+    // Adds a key named exit and adds its path
     commandMap.put("exit", "commands.Exit");
-
   }
 
   /**
@@ -66,21 +80,33 @@ public class CommandHandler {
    * @param parsedInput the parsed input containing the command, and its arguments
    */
   public void setCommand(String parsedInput) {
+    //Initializes an array containing the words of the parsedInput
     splitInput = parsedInput.split(" ");
+    //Retrieving the command portion of the the user input from array 
     command = splitInput[0];
+    //Retrieving the arguments portion of the the user input from array 
     String args[] = Arrays.copyOfRange(splitInput, 1, splitInput.length);
 
+    //If we are in speak mode
     if (speakMode) {
+      //Sets the command to the String speak
       this.command = "speak";
+      //Sets the arguments to the splitInput
       args = splitInput;
     }
+    
+    //If the command was speak and there was no user input
     if (command.equals("speak") && args.length == 0)
+      //The console enters into speak mode
       speakMode = true;
+    
+    //Calls the function to run the command
     run(command, args, parsedInput);
+    
+    //If the command was speak and the user input ends with the special keyword QUIT
     if (command.equals("speak") && parsedInput.endsWith("QUIT"))
+      //The console exits into speak mode
       speakMode = false;
-
-
   }
 
   /**
@@ -91,17 +117,24 @@ public class CommandHandler {
    * @param fullInput the raw input that the user gave to JShell
    */
   public void run(String command, String[] args, String fullInput) {
+    
+    //Check if the command is supported 
     if (!commandMap.containsKey(command)) {
-      output = errorManager.getError("Invalid Command", command);
+      //Sets the error as Invalid Command
+      output = errorManager.getError("Invalid Command", command + " is not supported");
     } else {
       try {
+        
+        //Gets the class path of the command that needs to ne run
         String className = commandMap.get(command);
 
         try {
 
+          //Created an instance of the Class and initialized it 
           CommandI commandObj = (CommandI) Class.forName(className)
               .getDeclaredConstructor().newInstance();
 
+          //Calls the run command in that respective class anc collects the output
           output = commandObj.run(args, fullInput, speakMode);
 
         } catch (InstantiationException e) {
@@ -121,6 +154,7 @@ public class CommandHandler {
         e.printStackTrace();
       }
     }
+   
     if (output != null)
       System.out.println(output);
   }
