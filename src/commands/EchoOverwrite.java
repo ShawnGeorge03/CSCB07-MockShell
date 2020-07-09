ab3ae6eb063d4e1ea42e57844bfd4747359a2aeb
ProgramTP <tirthpatelx@gmail.com>
2020-07-09 03:02:30 +0000

    added honor code and fixed jdoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@436 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/EchoOverwrite.java
+++ src/commands/EchoOverwrite.java
@@ -1,3 +1,32 @@
+// **********************************************************
+// Assignment2:
+// Student1:
+// UTORID user_name: patelt26
+// UT Student #: 1005904103
+// Author: Shawn Santhoshgeorge
+//
+// Student2:
+// UTORID user_name: shaiskan
+// UT Student #: 1006243940
+// Author: Keshavaa Shaiskandan
+//
+// Student3:
+// UTORID user_name: patelt26
+// UT Student #: 1005904103
+// Author: Tirth Patel
+//
+// Student4:
+// UTORID user_name: pate1101
+// UT Student #: 1006315765
+// Author: Abhay Patel
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences.
+// *********************************************************
 package commands;
 
 import data.Node;
@@ -10,8 +39,8 @@ public class EchoOverwrite extends Echo {
   /**
    * Takes contents and overwrites it to the contents of the requested file
    * 
-   * @param fileContents the contents that the user wants to overwrite with
-   * @param fileName name of the file
+   * @param fileContents  the contents that the user wants to overwrite with
+   * @param fileName  name of the file
    */
   public void execute(String fileContents, String fileName) {
     // Parses the user inputted path
