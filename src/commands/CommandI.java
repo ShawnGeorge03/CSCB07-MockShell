2068b8a8eba5f69e140eb3b1782ec18e2a0d9bbb
ProgramTP <tirthpatelx@gmail.com>
2020-07-09 02:38:55 +0000

    fixed jdoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@406 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/CommandI.java
+++ src/commands/CommandI.java
@@ -11,10 +11,9 @@ interface CommandI {
   * Runs the command based on the implementation of this method in the 
   * respective class
   * 
-  * @param args the string array of arguments
-  * @param fullInput the full line of input that the user gives into JShell
-  * @param val stores a boolean value
-  *
+  * @param args  the string array of arguments
+  * @param fullInput  the full line of input that the user gives into JShell
+  * @param val  stores a boolean value
   * @return any error message or valid output for user input
   */  
   public String run(String args[], String fullInput, boolean val);
