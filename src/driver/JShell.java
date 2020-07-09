c2af3addda571bd03fc78927072ceb77797ad7c7
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-09 02:50:55 +0000

    Fixed glitches in TestCases
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@421 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/driver/JShell.java
+++ src/driver/JShell.java
@@ -39,7 +39,7 @@ public class JShell {
 
   public static void main(String[] args) {
 
-    boolean testing = false;
+    boolean testing = true;
     
     if (testing) {
       TestCases tester = new TestCases();
