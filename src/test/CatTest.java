75a6e1f09df664e26498ae0c522b155ba7024ece
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-07-21 13:20:00 +0000

    Commented CatTest
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@509 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/test/CatTest.java
+++ src/test/CatTest.java
@@ -16,24 +16,49 @@ import data.FileSystem;
 import java.lang.reflect.Field;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+/**
+ * Class CatTest runs all the different test cases for Cat
+ */
 public class CatTest {
 
+    /**
+    * Declare instance of FileSystem so we can access the filesystem
+    */
     private static FileSystem fs;
+    /**
+    * Declare instance of Mkdir to make new directories
+    */
     private static Mkdir mkdir;
+    /**
+    * Declare instance of Echo to make new file
+    */
     private static Echo echo;
+    /**
+    * Declare instance of Cat to be tested
+    */
     private static Cat cat;
 
+    /**
+    * Declare two different instance of a String 
+    * objects called expected and actual 
+    */
     private static String expected, actual;
 
+    /**
+     * Sets up the test environment and initializes the instance variables
+     * 
+     * @throws Exception
+    */
     @BeforeClass
     public static void setup() throws Exception {
+        //Get the current FileSystem
         fs = FileSystem.getFileSys();
+        // Initializes a Mkdir Object
         mkdir = new Mkdir();
+        //Initializes a Echo Object
         echo = new Echo();
+        //Initializes a Cat Object
         cat = new Cat();
-
-        expected = "";
-        actual = "";
 
         // Sets up the C Folder
         mkdir.MakeDirectory("users".split(" "));
@@ -68,61 +93,112 @@ public class CatTest {
         echo.run("C/Sys/IO/Mouse".split(" "), "echo \"Mouse is in Mouse Folder\" > C/Sys/IO/Mouse/Presses", false);
     }
 
+    /**
+     * Destroys the FileSystem after all the testcases have run
+     * 
+     * @throws Exception
+    */
     @AfterClass
     public static void tearDown() throws Exception {
+        //Declares and initializes a Feild variable 
+        //to the fileSys variable in FileSystem
         Field feild = fs.getClass().getDeclaredField("fileSys");
+        //Allows the value of this variable in FileSystem to be accessed
         feild.setAccessible(true);
+        //Changes the value of the variable in FileSystem to null
         feild.set(null, null);
     }
 
+    /**
+     * Test A : User provides no input
+    */
     @Test
     public void testANoArgs(){
+        //Declares and initializes an empty array
         String[] emptyArr = {};
+        //Expected return from Cat
         expected = "Error : No parameters provided : ";
+        //Actual return from Cat after the operation has been run
         actual = cat.run(emptyArr, "cat ", false);
+        //Checks if the values are equal or not
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test B : File not Found , Absolute Path
+    */
     @Test
     public void testBFileNotFoundCase1(){
+        //Expected return from Cat
         expected = "Error: File Not Found : C/pics/picflex";
+        //Actual return from Cat after the operation has been run
         actual = cat.run("C/pics/picflex".split(" "), "cat C/pics/picflex", false);
+         //Checks if the values are equal or not
         assertEquals(expected, actual);
     }
-
+    /**
+     * Test C : File Exists Absolute Path
+    */
     @Test
     public void testCAbsolutePath(){
+        //Expected return from Cat
         expected = "this is a picturefile indeed";
+        //Actual return from Cat after the operation has been run
         actual = cat.run("C/pics/picfile".split(" "), "cat C/pics/picfile", false);
+        //Checks if the values are equal or not
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test D : File not Found , Relative Path
+    */
     @Test
     public void testDFileNotFoundCase2(){
+        //Expected return from Cat
         expected = "Error: File Not Found : A0";
+        //Actual return from Cat after the operation has been run
         actual = cat.run("A0".split(" "), "cat A0", false);
+        //Checks if the values are equal or not
         assertEquals(expected, actual);
     }
-
+    /**
+     * Test E : File Exists , Relative Path
+    */
     @Test
     public void testERelativePath(){
+        //Expected return from Cat
         expected = "Wow what a project";
+        //Actual return from Cat after the operation has been run
         actual = cat.run("A2".split(" "), "cat A2", false);
+        //Checks if the values are equal or not
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test F : Multiple Files of different file paths
+    */
     @Test
     public void testFMultipleFiles(){
+        //Expected return from Cat    
         expected = "Hello TA" + "\n" + "\n" + "\n" + "Wow what a project";
+        //Actual return from Cat after the operation has been run
         actual = cat.run("C/users/desktop/CSCB07 A2".split(" "), "cat C/users/desktop/CSCB07 A2", false);
+        //Checks if the values are equal or not
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test G : Multiple Files of different file paths but one File Not Found
+    */
     @Test
     public void testGFileNotFoundCase3(){
+        //Expected return from Cat
         expected =  "2+2=5" + "\n" + "\n" + "\n" + "Error: File Not Found : C/Sys/LOL"
-        + "\n" + "\n" + "\n" + "RGB == ways more      F    P   S";
-        actual = cat.run("C/users/desktop/Hwk C/Sys/LOL C/Sys/IO/keyboard/RGB".split(" "), "cat C/users/desktop/Hwk C/Sys/LOL C/Sys/IO/keyboard/RGB", false);
+                + "\n" + "\n" + "\n" + "RGB == ways more      F    P   S";
+        //Actual return from Cat after the operation has been run
+        actual = cat.run("C/users/desktop/Hwk C/Sys/LOL C/Sys/IO/keyboard/RGB".split(" "), 
+                "cat C/users/desktop/Hwk C/Sys/LOL C/Sys/IO/keyboard/RGB", false);
+        //Checks if the values are equal or not
         assertEquals(expected, actual);
     }
 
