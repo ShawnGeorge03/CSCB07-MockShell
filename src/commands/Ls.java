531b378a4e2c9459b117e34f0096287c53ed9cb1
skeshavaa <s.keshavaa@gmail.com>
2020-07-22 16:16:02 +0000

    Starting to work on Ls
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@520 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Ls.java
+++ src/commands/Ls.java
@@ -63,34 +63,53 @@ public class Ls extends DirectoryManager implements CommandI {
    */
   public String run(String[] arguments, String fullInput, boolean val) {
     this.args = new ArrayList<String>(Arrays.asList(arguments));
-    
-    if (args.size() == 0) {
-      Node curr = filesys.getCurrent();
-      for (int i = 0; i < curr.getList().size(); i++) {
-        System.out.println(curr.getList().get(i).getName());
-      }
-    } else {
-      for (int i = 0; i < args.size(); i++) {
-        String[] path = {args.get(i)};
-        String[] currentPath = {getCurrentPath()};
-
-        Cd traverse = new Cd();
-        if (traverse.run(path)) {
-          Node current = FileSystem.getFileSys().getCurrent();
-          System.out.println("Path: " + path[0]);
-          for (int j = 0; j < current.getList().size(); j++) {
-            System.out.println(current.getList().get(j).getName());
-          }
-        } else {
-          return error.getError("Invalid Directory",
-              args.get(i) + " is not a valid directory");
-        }
-
-        Cd goBack = new Cd();
-        goBack.run(currentPath);
-        System.out.println();
-      }
+    System.out.println("hello!");
+    if (args.get(0).equals("-R") != true) {
+    	unrecursiveMode();
+    }else {
+    	recursiveMode();
     }
-    return null;
-  }
+    return "hi";
+ }
+
+
+public String unrecursiveMode() {
+	if (args.size() == 0) {
+	      Node curr = filesys.getCurrent();
+	      for (int i = 0; i < curr.getList().size(); i++) {
+	        System.out.println(curr.getList().get(i).getName());
+	      }
+	    } else {
+	      for (int i = 0; i < args.size(); i++) {
+	        String[] path = {args.get(i)};
+	        String[] currentPath = {getCurrentPath()};
+
+	        Cd traverse = new Cd();
+	        if (traverse.run(path)) {
+	          Node current = FileSystem.getFileSys().getCurrent();
+	          System.out.println("Path: " + path[0]);
+	          for (int j = 0; j < current.getList().size(); j++) {
+	            System.out.println(current.getList().get(j).getName());
+	          }
+	        } else {
+	          return error.getError("Invalid Directory",
+	              args.get(i) + " is not a valid directory");
+	        }
+
+	        Cd goBack = new Cd();
+	        goBack.run(currentPath);
+	        System.out.println();
+	      }
+	    }
+	    return null;
+	}
+
+
+	public String recursiveMode() {
+		String output = "";
+		for (int i = 1; i < args.size(); i++) {
+			System.out.println(args.get(i));
+		}
+		return null;
+	}
 }
