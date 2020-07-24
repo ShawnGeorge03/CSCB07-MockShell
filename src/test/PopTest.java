4ff188e4e871a5d6f484b418138b375a52a587fd
skeshavaa <s.keshavaa@gmail.com>
2020-07-24 18:47:43 +0000

    fixed conflicts
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@570 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/test/PopTest.java
+++ src/test/PopTest.java
@@ -1,5 +1,89 @@
 package test;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+import commands.Cd;
+import commands.Mkdir;
+import commands.Pop;
+import commands.Push;
+import commands.Echo;
+import commands.DirectoryManager;
+import data.FileSystem;
+
+import java.lang.reflect.Field;
+
+
 public class PopTest {
     
+    private static FileSystem fs;
+
+    private static Mkdir mkdir;
+
+    private static Push push;
+
+    private static Pop pop;
+
+    private static DirectoryManager dirManager;
+
+    private static String[] input = {};
+
+    @Before
+    public void setUp() throws Exception {
+
+        fs = FileSystem.getFileSys();
+        mkdir = new Mkdir();
+        push = new Push();
+        pop = new Pop();
+        dirManager = new DirectoryManager();
+        
+        // Sets up the C Folder
+        mkdir.MakeDirectory("users".split(" "), fs);
+        mkdir.MakeDirectory("pics".split(" "), fs);
+        mkdir.MakeDirectory("Sys".split(" "), fs);
+
+        // Sets up the users Folder
+        mkdir.MakeDirectory("/users/desktop".split(" "), fs);
+
+        // Sets up the Sys Folder
+        mkdir.MakeDirectory("Sys/IO".split(" "), fs);
+        mkdir.MakeDirectory("Sys/LOL".split(" "), fs);
+
+        // Sets up the IO Folder
+        mkdir.MakeDirectory("/Sys/IO/keyboard".split(" "), fs);
+        mkdir.MakeDirectory("/Sys/IO/Mouse".split(" "), fs);
+
+        String[] path = {"users/desktop"};
+        push.run(fs, path, "pushd users/desktop", false);
+
+        path[0] = "//Sys/IO/keyboard";
+        push.run(fs, path, "pushd /Sys/IO/keyboard", false);
+    }
+
+     /**
+     * Destroys the FileSystem after all the testcases have run
+     * 
+     * @throws Exception
+    */
+    @After
+    public void tearDown() throws Exception {
+        //Declares and initializes a Feild variable 
+        //to the fileSys variable in FileSystem
+        Field feild = fs.getClass().getDeclaredField("fileSys");
+        //Allows the value of this variable in FileSystem to be accessed
+        feild.setAccessible(true);
+        //Changes the value of the variable in FileSystem to null
+        feild.set(null, null);
+    }
+
+    @Test
+    public void testAAbsolutePath(){
+        pop.run(fs, input, "popd", false);
+        String expected = "//Sys/IO/keyboard";
+        String actual = dirManager.getCurrentPath(fs);
+        assertEquals(expected, actual);
+    }
+
 }
