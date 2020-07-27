d52b556a5484f03284c6ee2c717543b17fe8ed4a
ProgramTP <tirthpatelx@gmail.com>
2020-07-27 21:00:56 +0000

    Added javadoc
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@600 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Tree.java
+++ src/commands/Tree.java
@@ -3,14 +3,32 @@ package commands;
 import data.FileSystemI;
 import data.Node;
 
+/**
+ * Class Tree is responsible for displaying all directories in the filesystem (including files)
+ */
 public class Tree implements CommandI {
-
+	
+	/**
+	 * Declare instance of errorhandler to handle error messages
+	 */
 	ErrorHandler e;
 
+	/**
+	 * Constructor for Tree which initializes errorhandler object
+	 */
 	public Tree() {
 		e = new ErrorHandler();
 	}
 
+	/**
+	 * Override run method from command interface which checks if arguments are valid, then runs the program
+	 * 
+	 * @param fs the filesystem interface we are using to access the filesystem
+	 * @param args the string array that contains the arguments the user has provided with the command
+	 * @param fullinput the string that contains the entire line of input provided to jshell
+	 * @param val the boolean we are using
+	 * @return any error messages there may be
+	 */
 	@Override
 	public String run(FileSystemI fs, String[] args, String fullInput, boolean val) {
 		if (!isValid(args)) {
@@ -21,7 +39,8 @@ public class Tree implements CommandI {
 		return null;
 	}
 	
-	public boolean isValid(String[] args) {
+	private boolean isValid(String[] args) {
+		//Check if any arguments were provided
 		if (args.length != 0) {
 			return false;
 		}
@@ -30,12 +49,16 @@ public class Tree implements CommandI {
 
 	private void traverse(Node root, int depth) {
 		String output = "";
+		//Print tabs depending on the depth of the location in filesystem
 		for (int i = 0; i < depth; i++) {
 			output += "\t";
 		}
+		//Add name of node that we are currently at and print it
 		output += root.getName();
 		System.out.println(output);
 		for (int i = 0; i < root.getList().size(); i++) {
+			//increment depth by 1 then recursively call the method to perform a DFS into
+			//the last node and then decrement the depth back
 			depth += 1;
 			traverse(root.getList().get(i), depth);
 			depth -= 1;
