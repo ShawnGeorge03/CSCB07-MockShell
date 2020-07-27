d14ee06f31472e9c655bb0da594d7fec2e5b9fed
ProgramTP <tirthpatelx@gmail.com>
2020-07-27 15:37:19 +0000

    Tree
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@588 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

---
+++ src/commands/Tree.java
@@ -0,0 +1,48 @@
+package commands;
+
+import data.FileSystemI;
+import data.Node;
+
+public class Tree implements CommandI {
+
+	ErrorHandler e;
+
+	public Tree() {
+		e = new ErrorHandler();
+	}
+
+	@Override
+	public String run(FileSystemI fs, String[] args, String fullInput, boolean val) {
+		int depth = 0;
+		traverse(depth, fs, fs.getRoot());
+
+		return null;
+	}
+
+	public void traverse(int depth, FileSystemI f, Node currDir) {
+		if (currDir.getisDir()) {
+			printDir(currDir, depth);
+		}
+		if (currDir.getList() != null) {
+			for (int i = 0; i < currDir.getList().size(); i++) {
+				traverse(depth + 1, f, currDir.getList().get(i));
+			}
+		}
+	}
+
+	public void printDir(Node dirToPrint, int depth) {
+		System.out.println("\t");
+		if (dirToPrint.getList() != null) {
+			for (int i = 0; i < dirToPrint.getList().size(); i++) {
+				System.out.println(dirToPrint.getList().get(i));
+			}
+		}
+	}
+
+//	public static void main(String[] args) {
+//		FileSystem fs = getFileSys();
+//		Tree t = new Tree();
+//		t.run(fs, null, null, true);
+//	}
+
+}
