79178d037c4e7021d4fd49f2c623783a1da13066
ProgramTP <tirthpatelx@gmail.com>
2020-08-02 00:36:04 +0000

    Added javadoc and added output into a string
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@703 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/commands/Tree.java
+++ src/commands/Tree.java
@@ -9,6 +9,9 @@ import errors.InvalidArgsProvidedException;
  */
 public class Tree implements CommandI {
 	
+	/**
+	 * Declaring instance variable for RedirectionManager
+	 */
 	RedirectionManager rManager;
 
 	/**
@@ -31,35 +34,47 @@ public class Tree implements CommandI {
 	public String run(FileSystemI fs, String fullInput, boolean val) {
 		//Seperates the parameters from everything else from the user input
 		String[] args = rManager.setParams(fullInput);
+		//Try catch to test validity of args
 		try {
 			checkArgs(fs, args, fullInput); 
 		} catch (InvalidArgsProvidedException e) {
 			return e.getLocalizedMessage();
 		}
-		traverse(fs.getRoot(), 0);
-		System.out.println("");
-		return null;
+		String output = "";
+		//Traverse file system and gather the desired tree format in the string output
+		output = traverse(fs.getRoot(), 0, output);
+		return output;
 	}
 
-	private void traverse(Node root, int depth) {
-		String output = "";
+	private String traverse(Node root, int depth, String output) {
 		//Print tabs depending on the depth of the location in filesystem
 		for (int i = 0; i < depth; i++) {
 			output += "\t";
 		}
 		//Add name of node that we are currently at and print it
 		output += root.getName();
-		System.out.println(output);
+		output += "\n";
+		//System.out.println(output);
 		for (int i = 0; i < root.getList().size(); i++) {
 			//increment depth by 1 then recursively call the method to perform a DFS into
 			//the last node and then decrement the depth back
 			depth += 1;
-			traverse(root.getList().get(i), depth);
+			output = traverse(root.getList().get(i), depth, output);
 			depth -= 1;
 		}
+		return output;
 	}
 
 	@Override
+	/**
+	 * Overridden method checkArgs which ensures that no arguments have been called for tree, as it requires no arguments
+	 * 
+	 * @param fs the variable we are using to access the filesystem
+	 * @param arguments the string array containing all the arguments provided alongside the command
+	 * @param fullInput the string that contains the raw input provided to console in jshell
+	 * @return boolean true if there were no arguments provided
+	 * @throws InvalidArgsProvidedException the user provided invalid argument
+	 */
 	public boolean checkArgs(FileSystemI fs, String[] arguments, String fullInput) throws InvalidArgsProvidedException {
 		//Check if any arguments were provided
 		if (arguments.length != 0) {
