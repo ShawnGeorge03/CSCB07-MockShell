ed376879ddfd98d17a582cf6f74e85846cb0e53a
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-08-02 01:29:46 +0000

    Added comments to all testcases
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@706 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/test/PopTest.java
+++ src/test/PopTest.java
@@ -9,22 +9,39 @@ import commands.Pop;
 
 import java.lang.reflect.Field;
 
-
 public class PopTest {
-    
+
+    /**
+     * Declare instance of FileSystem so we can access the filesystem
+     */
     private static MockFileSystem fs;
 
+    /**
+     * Declares instance of Pop to be tested
+     */
     private static Pop pop;
 
-    String actual, expected;
+    /**
+     * Declare two different instance of a String objects called expected and actual
+     */
+    private String actual, expected;
 
+    /**
+     * Sets up the test environment and initializes the instance variables
+     * 
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
+        // Gets a specific preset FileSystem
         fs = MockFileSystem.getMockFileSys("MOCKENV");
+        // Initializes the class to be tested
         pop = new Pop();
+        // Sets up the Directory Stack
         fs.getStack().add("/documents/journal");
         fs.getStack().add("/users/skeshavaa");
         fs.getStack().add("/");
+        // Sets the current working directory
         fs.setCurrent(fs.desktop);
     }
 
@@ -33,68 +50,98 @@ public class PopTest {
      * 
      * @throws Exception
      */
-  @After
-  public void tearDown() throws Exception {
-    // Declares and initializes a Feild variable
-    // to the fileSys variable in MockFileSystem
-    Field feild = fs.getClass().getDeclaredField("filesys");
-    // Allows the value of this variable in MockFileSystem to be accessed
-    feild.setAccessible(true);
-    // Changes the value of the variable in MockFileSystem to null
-    feild.set(null, null);
-  }
-
-    @Test
-    public void testAAbsolutePath(){
-        pop.run(fs, "popd", false);
-        expected = "/documents/journal";
-        actual = fs.getCurrentPath();
-        assertEquals(expected, actual);
+    @After
+    public void tearDown() throws Exception {
+        // Declares and initializes a Feild variable
+        // to the fileSys variable in MockFileSystem
+        Field feild = fs.getClass().getDeclaredField("filesys");
+        // Allows the value of this variable in MockFileSystem to be accessed
+        feild.setAccessible(true);
+        // Changes the value of the variable in MockFileSystem to null
+        feild.set(null, null);
     }
 
+    /**
+     * Test A : User calls pop once
+     */
     @Test
-    public void testBRelativePath(){
-        pop.run(fs,  "popd", false);
-        pop.run(fs,  "popd", false);
-        expected = "/users/skeshavaa";
-        actual = fs.getCurrentPath();
-        assertEquals(expected, actual);
+    public void testAAbsolutePath() {
+        //Expected return from Pop
+        expected = null;
+        //Actual return from Pop
+        actual = pop.run(fs, "popd", false);
+        //Check the return from Pop and the current working directory
+        assertTrue(actual == expected && fs.getCurrentPath().equals("/documents/journal"));
+    }
+    /**
+     * Test B : User calls pop twice
+     */
+    @Test
+    public void testBRelativePath() {
+        //Expected return from Pop
+        expected = null;
+        //Actual return from Pop
+        actual = pop.run(fs, "popd", false);
+        actual = pop.run(fs, "popd", false);
+        //Check the return from Pop and the current working directory
+        assertTrue(actual == expected && fs.getCurrentPath().equals("/users/skeshavaa"));
     }
 
+    /**
+     * Test C : User calls pop three times
+     */
     @Test
-    public void testCLastPop(){
-        pop.run(fs,  "popd", false);
-        pop.run(fs,  "popd", false);
-        pop.run(fs,  "popd", false);
-
-        expected = "/";;
-        actual = fs.getCurrentPath();
-        assertEquals(expected, actual);
+    public void testCLastPop() {
+        //Expected return from Pop
+        expected = null;
+        //Actual return from Pop
+        actual = pop.run(fs, "popd", false);
+        actual = pop.run(fs, "popd", false);
+        actual = pop.run(fs, "popd", false);
+        //Check the return from Pop and the current working directory
+        assertTrue(actual == expected && fs.getCurrentPath().equals("/"));
     }
 
+    /**
+     * Test D : User calls pop when the stack is empty
+     */
     @Test
-    public void testDEmptyStack(){
-        pop.run(fs,  "popd", false);
-        pop.run(fs,  "popd", false);
-        pop.run(fs,  "popd", false);
-       
-        expected = "Error: Stack is empty";;
-        actual =  pop.run(fs,  "popd", false);
-        assertEquals(expected, actual);
+    public void testDEmptyStack() {
+        //Expected return from Pop
+        expected = "Error: Stack is empty";
+        //Actual return from Pop
+        actual = pop.run(fs, "popd", false);
+        actual = pop.run(fs, "popd", false);
+        actual = pop.run(fs, "popd", false);
+        actual = pop.run(fs, "popd", false);
+        //Check the return from Pop and the current working directory
+        assertTrue(actual.equals(expected) && fs.getCurrentPath().equals("/")); 
     }
 
-    @Test 
-    public void testEArguments(){
-        actual = pop.run(fs, "popd unrequired args", false);
+    /**
+     * Test E : User provides arguments
+     */
+    @Test
+    public void testEArguments() {
+        //Expected return from Pop
         expected = "Error: Invalid Argument : No arguments should be given";
-        assertEquals(expected, actual);
+        //Actual return from Pop
+        actual = pop.run(fs, "popd unrequired args", false);
+        System.out.println(actual + "|" + fs.getCurrentPath());
+        //Check the return from Pop and the current working directory
+        assertTrue(actual.equals(expected) && fs.getCurrentPath().equals("/desktop"));  
     }
 
+    /**
+     * Test F : User tries to use redirection on a non redirectionable command
+     */
     @Test
-    public void testFRedirectionError(){
+    public void testFRedirectionError() {
+        //Expected return from Pop
         expected = "Error : Redirection Error : popd does not support redirection";
-        actual = pop.run(fs, "popd > test", false);
-        assertEquals(expected, actual);
+        //Actual return from Pop
+        actual  = pop.run(fs, "popd > test", false);
+        //Check the return from Pop and the current working directory
+        assertTrue(actual.equals(expected) && fs.getCurrentPath().equals("/desktop"));  
     }
-
 }
--- src/test/PushTest.java
+++ src/test/PushTest.java
@@ -9,84 +9,131 @@ import org.junit.After;
 import org.junit.Test;
 
 import commands.Push;
-import data.FileSystemI;
-
-
 
 public class PushTest {
-    
-    private static FileSystemI fs;
+
+    /**
+     * Declare instance of FileSystem so we can access the filesystem
+     */
+    private static MockFileSystem fs;
+
+    /**
+     * Declares instance of Push to be tested
+     */
     private static Push push;
 
-    String actual, expected;
+    /**
+     * Declare two different instance of a String objects called expected and actual
+     */
+    private String actual, expected;
 
+    /**
+     * Sets up the test environment and initializes the instance variables
+     * 
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
+        // Gets a specific preset FileSystem
         fs = MockFileSystem.getMockFileSys("MOCKENV");
+        // Initializes the class to be tested
         push = new Push();
     }
 
-  /**
-   * Destroys the MockFileSystem after each testcases have run
-   * 
-   * @throws Exception
-   */
-  @After
-  public void tearDown() throws Exception {
-    // Declares and initializes a Feild variable
-    // to the fileSys variable in MockFileSystem
-    Field feild = fs.getClass().getDeclaredField("filesys");
-    // Allows the value of this variable in MockFileSystem to be accessed
-    feild.setAccessible(true);
-    // Changes the value of the variable in MockFileSystem to null
-    feild.set(null, null);
-  }
+    /**
+     * Destroys the MockFileSystem after each testcases have run
+     * 
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        // Declares and initializes a Feild variable
+        // to the fileSys variable in MockFileSystem
+        Field feild = fs.getClass().getDeclaredField("filesys");
+        // Allows the value of this variable in MockFileSystem to be accessed
+        feild.setAccessible(true);
+        // Changes the value of the variable in MockFileSystem to null
+        feild.set(null, null);
+    }
 
+    /**
+     * Test A : User provides an relative directory path
+     */
     @Test
     public void testARelativePath() {
-        push.run(fs, "pushd users/skeshavaa", false);
-        expected = "/users/skeshavaa";
-        actual = fs.getCurrentPath();
-        assertEquals(expected, actual);
+        //Expected return from Push
+        expected = null;
+        //Actual return from Push
+        actual = push.run(fs, "pushd users/skeshavaa", false);
+        //Check the return from Push and the current working directory
+        assertTrue(actual == expected && fs.getCurrentPath().equals("/users/skeshavaa"));
     }
 
+    /**
+     * Test B : User provides an absolute directory path
+     */
     @Test
     public void testBAbsolutePath() {
-        push.run(fs, "pushd /documents/journal", false);
-        actual = fs.getCurrentPath();
-        expected = "/documents/journal";
-        assertEquals(expected, actual);
+        //Expected return from Push
+        expected = null;
+        //Actual return from Push
+        actual = push.run(fs, "pushd /documents/journal", false);
+        //Check the return from Push and the current working directory
+        assertTrue(actual == expected && fs.getCurrentPath().equals("/documents/journal"));
     }
 
+    /**
+     * Test C : User provides an invalid directory path
+     */
     @Test
     public void testCInvalidPath() {
-        actual = push.run(fs, "pushd wrongpath", false);
+        //Expected return from Push
         expected = "Error: Invalid Directory : wrongpath is not a valid directory";
-        assertEquals(expected, actual);
+        //Actual return from Push
+        actual = push.run(fs, "pushd wrongpath", false);
+        //Check the return from Push and the current working directory
+        assertTrue(actual.equals(expected) && fs.getCurrentPath().equals("/"));
     }
 
+    /**
+     * Test D : User provides no arguments
+     */
     @Test
     public void testDNoArguments() {
+        //Expected return from Push
+        expected = "Error : No parameters provided";
+        //Actual return from Push
         actual = push.run(fs, "pushd ", false);
-        expected ="Error : No parameters provided";
-        assertEquals(expected, actual);
+        //Check the return from Push and the current working directory
+        assertTrue(actual.equals(expected) && fs.getCurrentPath().equals("/"));
     }
 
+    /**
+     * Test E : User provides multiple paths
+     */
     @Test
     public void testEMultipleArguments() {
-        actual = push.run(fs, "pushd hi hello bye", false);
+        //Expected return from Push
         expected = "Error : Multiple Parameters have been provided : "
-                        +"hi hello bye Only 1 valid directory path is required";
-        assertEquals(expected, actual);
+        + "hi hello bye Only 1 valid directory path is required";
+        //Actual return from Push
+        actual = push.run(fs, "pushd hi hello bye", false);    
+        //Check the return from Push and the current working directory
+        assertTrue(actual.equals(expected) && fs.getCurrentPath().equals("/"));
     }
 
+
+    /**
+     * Test F : User tries to use redirection on a non redirectionable command
+     */
     @Test
-    public void testFRedirectionError(){
+    public void testFRedirectionError() {
+        //Expected return from Push
         expected = "Error : Redirection Error : pushd does not support redirection";
+        //Actual return from Push
         actual = push.run(fs, "pushd /users > test", false);
-        assertEquals(expected, actual);
+        //Check the return from Push and the current working directory
+        assertTrue(actual.equals(expected) && fs.getCurrentPath().equals("/"));
     }
 
-   
-
-}   
+}
