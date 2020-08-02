6225cfd36ffc9d93d8540e6446c8ff28add61026
ShawnGeorge03 <shawngeorge2001@gmail.com>
2020-08-02 20:08:19 +0000

    Commented Node
    git-svn-id: https://markus.utsc.utoronto.ca/svn/cscb07s20/group_0096/Assignment2@742 1a43b7c3-e839-48a6-87f1-e3a9b978dc18

--- src/data/Node.java
+++ src/data/Node.java
@@ -1,58 +1,89 @@
 package data;
 
 import java.util.ArrayList;
-
+/**
+ * Class Node is responsible for making directories and files
+ */
 public class Node {
 
+  /**
+   * All the information that will not
+   * be changed after the node is created
+   */
   private final boolean isDir;
   private final boolean isRoot;
   private final String name;
   private final ArrayList<Node> list;
   
+  /**
+   * All information that can be changed
+   * after the Node is created
+   */
   private String content;
   private Node parent;
 
+  /**
+   * Class Builder is resposible for
+   * creating the Node class based on 
+   * the arguments it recieves
+   */
   public static class Builder {
+    /**
+     * Required information for the Node
+     */
     private final String name;
     private final boolean isDir;
     
+    /**
+     * Optional information fro the Node
+     */
     private boolean isRoot = false;
     private String content = null;
     private Node parent = null;
     private ArrayList<Node> list = new ArrayList<Node>();
 
-    // Builder constructor with required fields (3)
+    // Builder constructor with required fields (2)
     public Builder(boolean isDir, String name) {
       this.name = name;
       this.isDir = isDir;
     }
 
+    //Optional Parameter
     public Builder setRoot(boolean isRoot) {
       this.isRoot = isRoot;
       return this;
     }
 
+    //Optional Parameter
     public Builder setContent(String content) {
       this.content = content;
       return this;
     }
 
+    //Optional Parameter
     public Builder setList(ArrayList<Node> list) {
       this.list = list;
       return this;
     }
 
+    //Optional Parameter
     public Builder setParent(Node parent) {
       this.parent = parent;
       return this;
     }
 
+    //Creates the Node object
     public Node build() {
       return new Node(this);
     }
-
   }
 
+  /**
+   * Assigns all the given 
+   * variable to the Node
+   * 
+   * @param b a Buil
+   */
   private Node(Builder b) {
     this.isDir = b.isDir;
     this.isRoot = b.isRoot;
@@ -62,6 +93,7 @@ public class Node {
     this.parent = b.parent;
 
   }
+
 
   public boolean getisDir() {
     return isDir;
